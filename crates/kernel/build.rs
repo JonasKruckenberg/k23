42e90c83124b6c5603b887969ff77412524f930a
Jonas Kruckenberg <iterpre@protonmail.com>
2023-11-17 13:26:45 +0100

    feat(kernel): add linker script & entry point
    The linker script is set up to ensure the entry point will end up at the OpenSBI specific start address (0x80020000).
    The entry point itself is rather barebones right now, only sending the hart into a wait-for-interrupt loop.

---
+++ crates/kernel/build.rs
@@ -0,0 +1,11 @@
+const LINKER: &[u8] = include_bytes!("riscv64-virt.ld");
+
+fn main() {
+    use std::{env, fs, path::PathBuf};
+
+    let ld = &PathBuf::from(env::var_os("OUT_DIR").unwrap()).join("linker.ld");
+    fs::write(ld, LINKER).unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed=LOG");
+    println!("cargo:rustc-link-arg=-T{}", ld.display());
+}
--- crates/kernel/src/main.rs
+++ crates/kernel/src/main.rs
@@ -1,7 +1,15 @@
 #![no_std]
 #![no_main]
+#![feature(naked_functions)]
 
 use core::arch::asm;
+
+#[link_section = ".text.start"]
+#[no_mangle]
+#[naked]
+unsafe extern "C" fn _start() -> ! {
+    asm!("1:", "wfi", "j 1b", options(noreturn))
+}
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
