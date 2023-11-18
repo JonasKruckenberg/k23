a358cb7d2e804e0caa980c45a85c7798b36697d7
Jonas Kruckenberg <iterpre@protonmail.com>
2023-11-18 12:13:28 +0100

    feat(kernel): setup stack & just into rust start function
    The hart stacks are laid out sequentially starting at the end of the BSS section (denoted by the __stack_start linker symbol) and are all STACK_SIZE_PAGES * PAGE_SIZE (i.e. 25 * 4096 bytes) in size aligned to 16 bytes as recommended/required by the riscv ISA. The hart ID effectively acts as an index into this slice of memory. This way we can set up the hart stack without needing a frame allocator early on.

--- crates/kernel/src/main.rs
+++ crates/kernel/src/main.rs
@@ -1,14 +1,38 @@
 #![no_std]
 #![no_main]
-#![feature(naked_functions)]
+#![feature(naked_functions, asm_const)]
 
 use core::arch::asm;
+
+const STACK_SIZE_PAGES: usize = 25;
+const PAGE_SIZE: usize = 4096;
 
 #[link_section = ".text.start"]
 #[no_mangle]
 #[naked]
 unsafe extern "C" fn _start() -> ! {
-    asm!("1:", "wfi", "j 1b", options(noreturn))
+    asm!(
+        "la     sp, __stack_start", // set the stack pointer to the bottom of the stack
+        "li     t0, {stack_size}", // load the stack size
+        "mv     t1, a0",            // load the hart id
+        "addi   t1, t1, 1", // add one to the hart id so that we add at least one stack size (stack grows from the top downwards)
+        "mul    t0, t0, t1", // multiply the stack size by the hart id to get the offset
+        "add    sp, sp, t0", // add the offset from sp to get the harts stack pointer
+
+        "jal zero, {start_rust}", // jump into Rust
+
+        stack_size = const STACK_SIZE_PAGES * PAGE_SIZE,
+        start_rust = sym start,
+        options(noreturn)
+    )
+}
+
+extern "C" fn start(hartid: usize, opaque: usize) -> ! {
+    loop {
+        unsafe {
+            asm!("wfi");
+        }
+    }
 }
 
 #[panic_handler]
