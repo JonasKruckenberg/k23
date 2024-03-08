e49d6152528add26814554e0b2f33b01acc19529
Jonas Kruckenberg <iterpre@protonmail.com>
2024-03-08 14:50:04 +0100

    refactor(loader): use SBI logging facilities

--- loader/src/logger.rs
+++ loader/src/logger.rs
@@ -1,25 +1,15 @@
-use crate::machine_info::MachineInfo;
+use core::fmt;
 use core::fmt::Write;
-use core::mem::MaybeUninit;
 use log::{LevelFilter, Metadata, Record};
-use spin::mutex::Mutex;
-use uart_16550::SerialPort;
+use spin::Mutex;
 
-static LOGGER: Logger = Logger(Mutex::new(MaybeUninit::uninit()));
+static LOGGER: Logger = Logger(Mutex::new(LoggerInner));
 
-struct Logger(Mutex<MaybeUninit<SerialPort>>);
+struct Logger(Mutex<LoggerInner>);
 
-pub fn init(machine_info: &MachineInfo) {
-    let serial_port = unsafe {
-        SerialPort::new(
-            machine_info.serial.reg.start,
-            machine_info.serial.clock_frequency,
-            38400,
-        )
-    };
+struct LoggerInner;
 
-    LOGGER.0.lock().write(serial_port);
-
+pub fn init() {
     log::set_logger(&LOGGER).unwrap();
     log::set_max_level(LevelFilter::Trace);
 }
@@ -31,22 +21,30 @@ impl log::Log for Logger {
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            let mut uart = self.0.lock();
-            let uart = unsafe { uart.assume_init_mut() };
+            let mut inner = self.0.lock();
 
-            // disable interrupts while we hold the uart lock
-            // otherwise we might deadlock if we try to log from the trap handler
-            // arch::without_interrupts(|| {
             let _ = writeln!(
-                uart,
+                inner,
                 "[{:<5} {}] {}",
                 record.level(),
                 record.module_path_static().unwrap_or_default(),
                 record.args()
             );
-            // });
         }
     }
 
     fn flush(&self) {}
+}
+
+impl Write for LoggerInner {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        cfg_if::cfg_if! {
+            if #[cfg(target_arch = "riscv64")] {
+                let ptr = s.as_ptr();
+                let _ = sbicall::dbcn::debug_console_write(s.len(), ptr as usize, 0);
+            }
+        }
+
+        Ok(())
+    }
 }
