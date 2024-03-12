b85512248a354f3e9ae9a1b5e42a8995cf8b9a35
Jonas Kruckenberg <iterpre@protonmail.com>
2024-03-12 10:51:53 +0100

    feat(xtask): `xtask check` and `xtask gdb` commands

--- build/xtask/src/main.rs
+++ build/xtask/src/main.rs
@@ -1,12 +1,12 @@
 mod logger;
 
 use anyhow::anyhow;
-use cargo_metadata::camino::Utf8PathBuf;
+use cargo_metadata::camino::{Utf8Path, Utf8PathBuf};
 use cargo_metadata::{Artifact, Message, MetadataCommand};
 use clap::{ArgAction, Parser};
 use std::ffi::OsStr;
 use std::path::PathBuf;
-use std::process::{Command, Stdio};
+use std::process::{Child, Command, Stdio};
 
 /// Helper for passing VERSION to opt.
 /// If `CARGO_VERSION_INFO` is set, use it, otherwise use `CARGO_PKG_VERSION`.
@@ -32,6 +32,20 @@ struct Common {
 
 #[derive(Debug, Parser)]
 enum XtaskCommand {
+    Check {
+        // /// Path to the image configuration file, in TOML.
+        // cfg: PathBuf,
+    },
+    Gdb {
+        // /// Path to the image configuration file, in TOML.
+        // cfg: PathBuf,
+        /// Whether to build in release mode instead of debug mode
+        #[clap(short, long)]
+        release: bool,
+        /// Don't start QEMU in the background
+        #[clap(long, default_value = "true")]
+        run: bool,
+    },
     /// Builds and runs the image in QEMU
     Run {
         // /// Path to the image configuration file, in TOML.
@@ -62,48 +76,41 @@ fn run() -> anyhow::Result<()> {
     logger::init(xtask.common.verbose);
 
     match xtask.cmd {
-        XtaskCommand::Run { release, .. } => {
-            // let loader = build_loader(release)?;
+        XtaskCommand::Check { .. } => {
+            check_kernel()?;
+            check_loader()?;
+        }
+        XtaskCommand::Gdb { release, run, .. } => {
             let kernel = build_kernel(release)?;
+            let loader = build_loader(release, &kernel)?;
 
-            // log::debug!("{loader}");
+            let maybe_child = if run {
+                let c = start_qemu("qemu-system-riscv64", loader.as_str(), true)?;
+                Some(c)
+            } else {
+                None
+            };
+
+            Command::new("rust-lldb")
+                .args([loader.as_str(), "-o", "gdb-remote localhost:1234"])
+                .stdout(Stdio::inherit())
+                .stdin(Stdio::inherit())
+                .stderr(Stdio::inherit())
+                .output()?;
+
+            if let Some(mut c) = maybe_child {
+                c.kill()?;
+            }
+        }
+        XtaskCommand::Run { release, .. } => {
+            let kernel = build_kernel(release)?;
+            let loader = build_loader(release, &kernel)?;
+
+            log::debug!("{loader}");
             log::debug!("{kernel}");
 
-            Command::new("qemu-system-riscv64")
-                .args([
-                    "-bios",
-                    "default",
-                    "-kernel",
-                    kernel.as_str(),
-                    "-machine",
-                    "virt",
-                    "-cpu",
-                    "rv64",
-                    "-d",
-                    "guest_errors,unimp",
-                    "-smp",
-                    "8",
-                    "-m",
-                    "128M",
-                    "-nographic",
-                    "-serial",
-                    "mon:stdio",
-                    "-device",
-                    "virtio-rng-device",
-                    "-device",
-                    "virtio-gpu-device",
-                    "-device",
-                    "virtio-net-device",
-                    "-device",
-                    "virtio-tablet-device",
-                    "-device",
-                    "virtio-keyboard-device",
-                ])
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .stdin(Stdio::inherit())
-                .output()
-                .unwrap();
+            let mut c = start_qemu("qemu-system-riscv64", loader.as_str(), false)?;
+            c.wait()?;
         }
         XtaskCommand::Dist { .. } => {
             todo!()
@@ -113,28 +120,47 @@ fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-// fn build_loader(release: bool) -> anyhow::Result<Utf8PathBuf> {
-//     let bootloader = Builder::new("loader", "riscv64imac-unknown-none-elf")
-//         .release(release)
-//         .env("RUSTFLAGS", "-Csoft-float")
-//         .additional_args([
-//             "-Z",
-//             "build-std=core,alloc",
-//             "-Z",
-//             "build-std-features=compiler-builtins-mem",
-//         ])
-//         .build()?;
-//
-//     let executable = bootloader
-//         .executable
-//         .ok_or(anyhow!("failed to retrieve kernel artifact"))?;
-//
-//     Ok(executable)
-// }
+fn check_loader() -> anyhow::Result<()> {
+    Cargo::new_check("loader", "riscv64imac-unknown-none-elf")
+        .env("RUSTFLAGS", "-Zstack-protector=all -Csoft-float=true")
+        .env("K23_KERNEL_ARTIFACT", "")
+        .exec()?;
+
+    Ok(())
+}
+
+fn check_kernel() -> anyhow::Result<()> {
+    Cargo::new_check("kernel", "riscv64gc-unknown-none-elf")
+        .env("RUSTFLAGS", "-Zstack-protector=all")
+        .exec()?;
+
+    Ok(())
+}
+
+fn build_loader(release: bool, kernel: &Utf8Path) -> anyhow::Result<Utf8PathBuf> {
+    let bootloader = Cargo::new_build("loader", "riscv64imac-unknown-none-elf")
+        .release(release)
+        .env("RUSTFLAGS", "-Zstack-protector=all -Csoft-float=true")
+        .env("K23_KERNEL_ARTIFACT", kernel)
+        .additional_args([
+            "-Z",
+            "build-std=core,alloc",
+            "-Z",
+            "build-std-features=compiler-builtins-mem",
+        ])
+        .exec()?;
+
+    let executable = bootloader
+        .executable
+        .ok_or(anyhow!("failed to retrieve kernel artifact"))?;
+
+    Ok(executable)
+}
 
 fn build_kernel(release: bool) -> anyhow::Result<Utf8PathBuf> {
-    let kernel = Builder::new("kernel", "riscv64gc-unknown-none-elf")
+    let kernel = Cargo::new_build("kernel", "riscv64gc-unknown-none-elf")
         .release(release)
+        .env("RUSTFLAGS", "-Zstack-protector=all")
         // .env(
         //     "RUSTFLAGS",
         //     "-Cforce-unwind-tables=true -Zstack-protector=strong",
@@ -145,7 +171,7 @@ fn build_kernel(release: bool) -> anyhow::Result<Utf8PathBuf> {
             "-Z",
             "build-std-features=compiler-builtins-mem",
         ])
-        .build()?;
+        .exec()?;
 
     let executable = kernel
         .executable
@@ -154,37 +180,45 @@ fn build_kernel(release: bool) -> anyhow::Result<Utf8PathBuf> {
     Ok(executable)
 }
 
-struct Builder<'a> {
-    name: &'a str,
+struct Cargo<'a> {
+    crate_name: &'a str,
     release: bool,
     features: Vec<String>,
     command: Command,
 }
 
-impl<'a> Builder<'a> {
-    pub fn new(name: &'a str, target: &str) -> Self {
+impl<'a> Cargo<'a> {
+    pub fn new_build(crate_name: &'a str, target: &str) -> Self {
+        Self::new("build", crate_name, target)
+    }
+
+    pub fn new_check(crate_name: &'a str, target: &str) -> Self {
+        Self::new("check", crate_name, target)
+    }
+
+    fn new(cmd: &'a str, crate_name: &'a str, target: &str) -> Self {
         let mut command = Command::new("cargo");
         command.args([
-            "build",
+            cmd,
             "--message-format=json-render-diagnostics",
             "-p",
-            name,
+            crate_name,
             "--target",
             target,
         ]);
 
         Self {
-            name,
+            crate_name,
             release: false,
             features: vec![],
             command,
         }
     }
 
-    // pub fn env(mut self, key: impl AsRef<OsStr>, val: impl AsRef<OsStr>) -> Self {
-    //     self.command.env(key, val);
-    //     self
-    // }
+    pub fn env(mut self, key: impl AsRef<OsStr>, val: impl AsRef<OsStr>) -> Self {
+        self.command.env(key, val);
+        self
+    }
 
     pub fn additional_args<I, A>(mut self, args: I) -> Self
     where
@@ -200,7 +234,7 @@ impl<'a> Builder<'a> {
         self
     }
 
-    pub fn build(mut self) -> anyhow::Result<Artifact> {
+    pub fn exec(mut self) -> anyhow::Result<Artifact> {
         self.command.args(["--features", &self.features.join(",")]);
         if self.release {
             self.command.arg("--release");
@@ -233,18 +267,63 @@ impl<'a> Builder<'a> {
                 }
                 Message::BuildFinished(finished) => {
                     if finished.success {
-                        log::info!("Successfully compiled `{}`", self.name);
+                        log::info!("Successfully compiled `{}`", self.crate_name);
                     } else {
-                        anyhow::bail!("could not compile `{}`", self.name);
+                        anyhow::bail!("could not compile `{}`", self.crate_name);
                     }
                 }
                 _ => (), // Unknown message
             }
         }
-        log::debug!("here");
 
         command.wait().expect("Couldn't get cargo's exit status");
 
         artifact.ok_or(anyhow!("failed to retrieve artifact from command"))
     }
+}
+
+fn start_qemu(runner: &str, kernel: &str, debug: bool) -> anyhow::Result<Child> {
+    let mut cmd = Command::new(runner);
+    cmd.args([
+        "-bios",
+        "default",
+        "-kernel",
+        kernel,
+        "-machine",
+        "virt",
+        "-cpu",
+        "rv64",
+        "-d",
+        "guest_errors,unimp",
+        "-smp",
+        "8",
+        "-m",
+        "128M",
+        "-nographic",
+        "-serial",
+        "mon:stdio",
+        "-device",
+        "virtio-rng-device",
+        "-device",
+        "virtio-gpu-device",
+        "-device",
+        "virtio-net-device",
+        "-device",
+        "virtio-tablet-device",
+        "-device",
+        "virtio-keyboard-device",
+    ]);
+
+    if debug {
+        cmd.args(["-s", "-S"]);
+        cmd.stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .stdin(Stdio::piped());
+    } else {
+        cmd.stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .stdin(Stdio::inherit());
+    }
+
+    Ok(cmd.spawn()?)
 }
