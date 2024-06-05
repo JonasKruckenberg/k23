e9ce5817d7484179138c3396260a7bdb75d25205
Jonas Kruckenberg <iterpre@protonmail.com>
2024-06-05 12:11:58 +0200

    cleanup

--- loader/src/arch/mod.rs
+++ loader/src/arch/mod.rs
@@ -2,7 +2,5 @@ cfg_if::cfg_if! {
     if #[cfg(target_arch = "riscv64")] {
         mod riscv64;
         pub use riscv64::*;
-    } else {
-        compile_error!("Unsupported target architecture");
     }
 }
