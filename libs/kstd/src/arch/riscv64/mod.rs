c19d89bddc5c27b8d7712a04e77d8d93f4642d16
Jonas Kruckenberg <iterpre@protonmail.com>
2024-06-16 10:58:47 +0200

    feat(kstd): exit emulator on panic if available

--- libs/kstd/src/arch/riscv64/mod.rs
+++ libs/kstd/src/arch/riscv64/mod.rs
@@ -7,6 +7,9 @@ use core::arch::asm;
 pub use register::*;
 
 pub fn abort_internal() -> ! {
+    semihosting::exit(1);
+
+    // fall back to a wfi loop if exiting using semihosting failed
     unsafe {
         loop {
             asm!("wfi");
