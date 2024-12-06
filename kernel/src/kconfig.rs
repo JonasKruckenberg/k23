c3a52ff0fc82692ea29cf241f6aa31a69eaa1cd9
Jonas Kruckenberg <iterpre@protonmail.com>
2024-12-06 17:13:16 +0100

    refactor: cleanup kernel for refactor

--- kernel/src/kconfig.rs
+++ kernel/src/kconfig.rs
@@ -6,6 +6,3 @@ pub const STACK_SIZE_PAGES: u32 = 256;
 pub const TRAP_STACK_SIZE_PAGES: usize = 16;
 /// The size of the kernel heap in pages
 pub const HEAP_SIZE_PAGES: u32 = 8192; // 32 MiB
-#[allow(non_camel_case_types)]
-pub type MEMORY_MODE = kmm::Riscv64Sv39;
-pub const PAGE_SIZE: usize = <MEMORY_MODE as kmm::Mode>::PAGE_SIZE;
--- kernel/src/start.rs
+++ kernel/src/start.rs
@@ -1,10 +1,5 @@
-use crate::{allocator, arch, frame_alloc, kconfig};
-use alloc::boxed::Box;
-use alloc::string::String;
-use core::any::Any;
+use crate::{arch, kconfig};
 use core::mem;
-use core::ops::Range;
-use kmm::{BitMapAllocator, Flush, Mapper, VirtualAddress};
 use loader_api::LoaderConfig;
 use sync::OnceLock;
 
@@ -49,44 +44,44 @@ fn pre_init_hart(hartid: usize) {
     // setup trap handler
 }
 
-fn init(boot_info: &'static loader_api::BootInfo) {
+fn init(_boot_info: &'static loader_api::BootInfo) {
     semihosting_logger::init(kconfig::LOG_LEVEL.to_level_filter());
 
-    log::debug!("initializing frame alloc...");
-    frame_alloc::init(boot_info, |alloc| -> Result<(), kmm::Error> {
-        log::debug!("Unmapping loader region {:?}...", boot_info.loader_region);
-        unmap_loader(alloc, boot_info.loader_region.clone())?;
+    // log::debug!("initializing frame alloc...");
+    // frame_alloc::init(boot_info, |alloc| -> Result<(), pmm::Error> {
+    //     // log::debug!("Unmapping loader region {:?}...", boot_info.loader_region);
+    //     // unmap_loader(alloc, boot_info.loader_region.clone())?;
+    //
+    //     // log::debug!("Setting up kernel heap...");
+    //     // allocator::init(alloc, boot_info)?;
+    //
+    //     Ok(())
+    // })
+    // .expect("failed to initialize frame allocator");
 
-        log::debug!("Setting up kernel heap...");
-        allocator::init(alloc, boot_info)?;
-
-        Ok(())
-    })
-    .expect("failed to initialize frame allocator");
-
-    panic_unwind::set_hook(Box::new(|info| {
-        let location = info.location();
-        let msg = payload_as_str(info.payload());
-
-        log::error!("hart panicked at {location}:\n{msg}");
-
-        // TODO this deadlocks :///
-        // let elf = unsafe {
-        //     let start = boot_info
-        //         .physical_memory_offset
-        //         .add(boot_info.kernel_elf.start.as_raw())
-        //         .as_raw() as *const u8;
-        //     slice::from_raw_parts(start, boot_info.kernel_elf.size())
-        // };
-        // let elf = ElfFile64::parse(elf).unwrap();
-        //
-        // let ctx =
-        //     SymbolizeContext::new(elf, boot_info.kernel_image_offset.as_raw() as u64).unwrap();
-        //
-        // let backtrace = Backtrace::capture(&ctx);
-        //
-        // log::error!("{backtrace}");
-    }));
+    // panic_unwind::set_hook(Box::new(|info| {
+    //     let location = info.location();
+    //     let msg = payload_as_str(info.payload());
+    //
+    //     log::error!("hart panicked at {location}:\n{msg}");
+    //
+    //     // TODO this deadlocks :///
+    //     // let elf = unsafe {
+    //     //     let start = boot_info
+    //     //         .physical_memory_offset
+    //     //         .add(boot_info.kernel_elf.start.as_raw())
+    //     //         .as_raw() as *const u8;
+    //     //     slice::from_raw_parts(start, boot_info.kernel_elf.size())
+    //     // };
+    //     // let elf = ElfFile64::parse(elf).unwrap();
+    //     //
+    //     // let ctx =
+    //     //     SymbolizeContext::new(elf, boot_info.kernel_image_offset.as_raw() as u64).unwrap();
+    //     //
+    //     // let backtrace = Backtrace::capture(&ctx);
+    //     //
+    //     // log::error!("{backtrace}");
+    // }));
 
     log::info!("Welcome to k23 {}", env!("CARGO_PKG_VERSION"));
 }
@@ -95,27 +90,27 @@ fn post_init_hart() {
     // enable interrupts
 }
 
-fn unmap_loader(
-    alloc: &mut BitMapAllocator<kconfig::MEMORY_MODE>,
-    loader_region: Range<VirtualAddress>,
-) -> Result<(), kmm::Error> {
-    let mut mapper: Mapper<kconfig::MEMORY_MODE> = Mapper::from_active(0, alloc);
-    let mut flush = Flush::empty(0);
+// fn unmap_loader(
+//     alloc: &mut BitMapAllocator,
+//     loader_region: Range<VirtualAddress>,
+// ) -> Result<(), pmm::Error> {
+//     // let mut mapper: Mapper<kconfig::MEMORY_MODE> = Mapper::from_active(0, alloc);
+//     // let mut flush = Flush::empty(0);
+//
+//     // Unmap the loader regions
+//     // mapper.unmap_forget_range(loader_region, &mut flush)?;
+//
+//     // flush.flush()?;
+//
+//     Ok(())
+// }
 
-    // Unmap the loader regions
-    mapper.unmap_forget_range(loader_region, &mut flush)?;
-
-    flush.flush()?;
-
-    Ok(())
-}
-
-fn payload_as_str(payload: &dyn Any) -> &str {
-    if let Some(&s) = payload.downcast_ref::<&'static str>() {
-        s
-    } else if let Some(s) = payload.downcast_ref::<String>() {
-        s.as_str()
-    } else {
-        "Box<dyn Any>"
-    }
-}
+// fn payload_as_str(payload: &dyn Any) -> &str {
+//     if let Some(&s) = payload.downcast_ref::<&'static str>() {
+//         s
+//     } else if let Some(s) = payload.downcast_ref::<String>() {
+//         s.as_str()
+//     } else {
+//         "Box<dyn Any>"
+//     }
+// }
