d3ca827d3ddc98e088b21669635f82d770a88d31
Jonas Kruckenberg <iterpre@protonmail.com>
2024-12-26 19:39:13 +0100

    fix startup

--- kernel/src/start.rs
+++ kernel/src/start.rs
@@ -1,8 +1,13 @@
 use crate::machine_info::{HartLocalMachineInfo, MachineInfo};
-use crate::{allocator, arch, vm, HEAP_SIZE_PAGES, LOG_LEVEL, STACK_SIZE_PAGES};
+use crate::{allocator, arch, logger, vm, HEAP_SIZE_PAGES, LOG_LEVEL, STACK_SIZE_PAGES};
+use alloc::boxed::Box;
+use alloc::string::String;
+use backtrace::{Backtrace, SymbolizeContext};
+use core::any::Any;
 use core::{mem, slice};
 use loader_api::{LoaderConfig, MemoryRegionKind};
-use sync::OnceLock;
+use mmu::AddressRangeExt;
+use sync::{LazyLock, Mutex, OnceLock};
 use thread_local::declare_thread_local;
 
 const LOADER_CFG: LoaderConfig = {
@@ -14,38 +19,22 @@ const LOADER_CFG: LoaderConfig = {
 
 pub static BOOT_INFO: OnceLock<&'static loader_api::BootInfo> = OnceLock::new();
 pub static MACHINE_INFO: OnceLock<MachineInfo> = OnceLock::new();
-
 declare_thread_local!(pub static HART_LOCAL_MACHINE_INFO: HartLocalMachineInfo);
 
 #[loader_api::entry(LOADER_CFG)]
 fn start(hartid: usize, boot_info: &'static loader_api::BootInfo) -> ! {
     panic_unwind::catch_unwind(|| {
-        let fdt = unsafe {
-            let fdt = slice::from_raw_parts(boot_info.memory_regions, boot_info.memory_regions_len)
-                .iter()
-                .find(|region| region.kind == MemoryRegionKind::FDT)
-                .expect("no FDT region");
+        let fdt = locate_device_tree(boot_info);
 
-            boot_info
-                .physical_address_offset
-                .add(fdt.range.start.as_raw())
-                .as_raw() as *const u8
-        };
+        hart_init_early(hartid, fdt);
 
-        begin_hart_init(hartid, fdt).unwrap();
+        sync::Once::default().call_once(|| init(boot_info, fdt));
 
-        BOOT_INFO.get_or_init(|| {
-            // reuse OnceLock for one-time initialization
-            init(boot_info, fdt).unwrap();
-
-            boot_info
-        });
-
-        finish_hart_init();
+        hart_init_late();
     })
     .unwrap_or_else(|e| {
         mem::forget(e);
-        log::error!("failed to initialize");
+        log::error!("system initialization failed");
         arch::abort();
     });
 
@@ -53,65 +42,70 @@ fn start(hartid: usize, boot_info: &'static loader_api::BootInfo) -> ! {
         fn kmain(hartid: usize, boot_info: &'static loader_api::BootInfo) -> !;
     }
 
-    panic_unwind::catch_unwind(|| unsafe { kmain(hartid, boot_info) }).unwrap_or_else(|_| {
+    panic_unwind::catch_unwind(|| {
+        backtrace::__rust_begin_short_backtrace(|| unsafe { kmain(hartid, boot_info) })
+    })
+    .unwrap_or_else(|e| {
+        mem::forget(e);
         log::error!("unrecoverable failure");
         arch::abort();
     })
 }
 
-fn begin_hart_init(hartid: usize, fdt: *const u8) -> crate::Result<()> {
+/// Early per-hart initialization.
+///
+/// This function gets called *before* the rest of the system initialization and can therefore only
+/// rely on the environment setup by the `loader`: interrupts are disabled, the initial page table is
+/// set up, stacks are initialized and the kernels ELF is fully mapped (including thread-local storage).
+///
+/// Notably, this function **cannot** use the logger and global allocator yet.
+fn hart_init_early(hartid: usize, fdt: *const u8) {
     logger::init_hart(hartid);
+    arch::hart_init_early();
     arch::trap_handler::init();
 
-    let minfo = unsafe { HartLocalMachineInfo::from_dtb(hartid, fdt)? };
+    let minfo = unsafe { HartLocalMachineInfo::from_dtb(hartid, fdt).unwrap() };
     HART_LOCAL_MACHINE_INFO.initialize_with(minfo, |_, _| {});
-
-    Ok(())
 }
 
-fn init(boot_info: &'static loader_api::BootInfo, fdt: *const u8) -> crate::Result<()> {
+fn init(boot_info: &'static loader_api::BootInfo, fdt: *const u8) {
     logger::init(LOG_LEVEL.to_level_filter());
 
+    BOOT_INFO.get_or_init(|| boot_info);
     log::debug!("\n{boot_info}");
 
-    log::debug!("Setting up kernel heap...");
+    log::trace!("Setting up kernel heap...");
     allocator::init(boot_info);
 
-    let minfo = MACHINE_INFO.get_or_try_init(|| unsafe { MachineInfo::from_dtb(fdt) })?;
-    log::trace!("\n{minfo}");
+    init_panic_hook();
+
+    let minfo = MACHINE_INFO
+        .get_or_try_init(|| unsafe { MachineInfo::from_dtb(fdt) })
+        .unwrap();
+    log::debug!("\n{minfo}");
 
     log::debug!("Setting up kernel virtual address space...");
-    vm::init(boot_info, minfo)?;
-
-    // panic_unwind::set_hook(Box::new(|info| {
-    //     let location = info.location();
-    //     let msg = payload_as_str(info.payload());
-    //
-    //     log::error!("hart panicked at {location}:\n{msg}");
-    //
-    //     // TODO this deadlocks :///
-    //     // let elf = unsafe {
-    //     //     let start = boot_info
-    //     //         .physical_memory_offset
-    //     //         .add(boot_info.kernel_elf.start.as_raw())
-    //     //         .as_raw() as *const u8;
-    //     //     slice::from_raw_parts(start, boot_info.kernel_elf.size())
-    //     // };
-    //     // let elf = ElfFile64::parse(elf).unwrap();
-    //     //
-    //     // let ctx =
-    //     //     SymbolizeContext::new(elf, boot_info.kernel_image_offset.as_raw() as u64).unwrap();
-    //     //
-    //     // let backtrace = Backtrace::capture(&ctx);
-    //     //
-    //     // log::error!("{backtrace}");
-    // }));
+    vm::init(boot_info, minfo).unwrap();
 
     log::info!("Welcome to k23 {}", env!("CARGO_PKG_VERSION"));
-
-    Ok(())
 }
 
-fn finish_hart_init() {
-    arch::finish_hart_init()
+/// Late per-hart initialization.
+///
+/// This function gets called *after* the kernel systems are initialized.
+fn hart_init_late() {
+    arch::hart_init_late();
+}
+
+fn locate_device_tree(boot_info: &'static loader_api::BootInfo) -> *const u8 {
+    let fdt = boot_info
+        .memory_regions()
+        .iter()
+        .find(|region| region.kind == MemoryRegionKind::FDT)
+        .expect("no FDT region");
+
+    boot_info
+        .physical_address_offset
+        .add(fdt.range.start.as_raw())
+        .as_raw() as *const u8
 }
