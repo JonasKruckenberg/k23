JonasKruckenberg/k23

commit 429b5c4556416ec79e0ad1c86a3423b7c58d1d72

wip: Wasm test functions (#385)
* wip

* Update wasm_spec.rs

* preflight

* cleanup tests

kernel/src/scheduler/idle.rs
@@ -5,7 +5,7 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use crate::arch;
+use crate::scheduler::park::{ParkToken, UnparkToken};
 use alloc::vec::Vec;
 use core::sync::atomic::{AtomicUsize, Ordering};
 use spin::Mutex;
@@ -20,7 +20,7 @@ pub struct Idle {
     /// Total number of cores
     num_cores: usize,
     /// Worker IDs that are currently sleeping
-    sleepers: Mutex<Vec<usize>>,
+    sleepers: Mutex<Vec<UnparkToken>>,
 }
 
 pub(crate) struct IdleMap {
@@ -42,7 +42,7 @@ impl Idle {
         self.num_searching.load(Ordering::Acquire)
     }
 
-    pub fn transition_worker_to_waiting(&self, worker: &super::Worker) {
+    pub fn transition_worker_to_waiting(&self, worker: &super::Worker) -> ParkToken {
         // tracing::trace!("Idle::transition_worker_to_waiting");
 
         // The worker should not be stealing at this point
@@ -56,8 +56,10 @@ impl Idle {
         let prev = self.num_idle.fetch_add(1, Ordering::Release);
         debug_assert!(prev < self.num_cores);
 
-        // Store the worker index in the list of sleepers
-        self.sleepers.lock().push(worker.cpuid);
+        let park = ParkToken::new(worker.cpuid);
+        self.sleepers.lock().push(park.clone().into_unpark());
+
+        park
     }
 
     pub fn transition_worker_from_waiting(&self, worker: &super::Worker) {
@@ -71,7 +73,7 @@ impl Idle {
 
         self.sleepers
             .lock()
-            .retain(|sleeper| *sleeper != worker.cpuid);
+            .retain(|sleeper| sleeper.cpuid() != worker.cpuid);
     }
 
     pub fn try_transition_worker_to_searching(&self, worker: &mut super::Worker) {
@@ -106,20 +108,14 @@ impl Idle {
     pub fn notify_one(&self) {
         // tracing::trace!("Idle::notify_one");
         if let Some(worker) = self.sleepers.lock().pop() {
-            // Safety: the worker placed itself into the sleepers list, so sending a wakeup is safe
-            unsafe {
-                arch::cpu_unpark(worker);
-            }
+            worker.unpark();
         }
     }
 
     pub fn notify_all(&self) {
         // tracing::trace!("Idle::notify_all");
         while let Some(worker) = self.sleepers.lock().pop() {
-            // Safety: the worker placed itself into the sleepers list, so sending a wakeup is safe
-            unsafe {
-                arch::cpu_unpark(worker);
-            }
+            worker.unpark();
         }
     }
 }

kernel/src/scheduler/mod.rs
@@ -63,26 +63,30 @@
 //! [`MultiThreadAlt`]: https://docs.rs/tokio/latest/tokio/runtime/struct.Builder.html#method.new_multi_thread_alt
 
 mod idle;
+mod park;
 mod queue;
 mod yield_now;
 
 use crate::cpu_local::CpuLocal;
-use crate::mem::AddressSpace;
+use crate::mem::with_kernel_aspace;
 use crate::scheduler::idle::Idle;
+use crate::scheduler::park::ParkToken;
 use crate::scheduler::queue::Overflow;
+use crate::task;
 use crate::task::{JoinHandle, OwnedTasks, PollResult, Schedule, TaskRef};
 use crate::time::Timer;
 use crate::util::fast_rand::FastRand;
-use crate::{arch, task};
-use alloc::sync::Arc;
 use core::any::type_name;
 use core::cell::{Ref, RefCell};
 use core::future::Future;
 use core::mem;
 use core::ops::DerefMut;
+use core::pin::pin;
 use core::sync::atomic::{AtomicBool, Ordering};
+use core::task::{Context, Poll};
+use cpu_local::cpu_local;
 use rand::RngCore;
-use spin::{Backoff, Barrier, Mutex, OnceLock};
+use spin::{Backoff, Barrier, OnceLock};
 
 const DEFAULT_GLOBAL_QUEUE_INTERVAL: u32 = 61;
 
@@ -182,11 +186,7 @@ pub struct Worker {
 
 impl Scheduler {
     #[track_caller]
-    pub fn spawn<F>(
-        &'static self,
-        aspace: Arc<Mutex<AddressSpace>>,
-        future: F,
-    ) -> JoinHandle<F::Output>
+    pub fn spawn<F>(&'static self, future: F) -> JoinHandle<F::Output>
     where
         F: Future + Send + 'static,
         F::Output: Send + 'static,
@@ -203,13 +203,41 @@ impl Scheduler {
             loc.col = loc.column(),
         );
 
-        let (handle, maybe_task) = self.owned.bind(future, self, id, span, aspace);
+        let (handle, maybe_task) =
+            with_kernel_aspace(|aspace| self.owned.bind(future, self, id, span, aspace.clone()));
 
         if let Some(task) = maybe_task {
             self.schedule(task);
         }
 
         handle
+    }
+
+    #[track_caller]
+    pub fn block_on<F>(&'static self, future: F) -> F::Output
+    where
+        F: Future + 'static,
+        F::Output: 'static,
+    {
+        cpu_local! {
+            static PARK: ParkToken = ParkToken::new(crate::CPUID.get());
+        }
+
+        let waker = PARK.with(|park| park.clone().into_unpark().into_waker());
+        let mut cx = Context::from_waker(&waker);
+
+        let mut future = pin!(future);
+
+        loop {
+            if let Poll::Ready(v) = future.as_mut().poll(&mut cx) {
+                return v;
+            }
+
+            // If there is only one CPU in the system we cannot go to sleep
+            if self.cores.len() > 1 {
+                PARK.with(|park| park.park());
+            }
+        }
     }
 
     pub fn shutdown(&self) {
@@ -320,11 +348,9 @@ impl Worker {
 
             // if we have no tasks to run, we can sleep until an interrupt
             // occurs.
-            self.scheduler.idle.transition_worker_to_waiting(self);
-            // Safety: we park the
-            unsafe {
-                arch::cpu_park();
-            }
+            let park = self.scheduler.idle.transition_worker_to_waiting(self);
+            park.park();
+
             self.scheduler.idle.transition_worker_from_waiting(self);
         }
 

kernel/src/mem/mod.rs
@@ -42,17 +42,16 @@ pub const KIB: usize = 1024;
 pub const MIB: usize = KIB * 1024;
 pub const GIB: usize = MIB * 1024;
 
-pub static KERNEL_ASPACE: OnceLock<Arc<Mutex<AddressSpace>>> = OnceLock::new();
+static KERNEL_ASPACE: OnceLock<Arc<Mutex<AddressSpace>>> = OnceLock::new();
 
 pub fn with_kernel_aspace<F, R>(f: F) -> R
 where
-    F: FnOnce(&mut AddressSpace) -> R,
+    F: FnOnce(&Arc<Mutex<AddressSpace>>) -> R,
 {
-    let mut aspace = KERNEL_ASPACE
+    let aspace = KERNEL_ASPACE
         .get()
-        .expect("kernel address space not initialized")
-        .lock();
-    f(&mut aspace)
+        .expect("kernel address space not initialized");
+    f(aspace)
 }
 
 pub fn init(
@@ -75,7 +74,7 @@ pub fn init(
         reserve_wired_regions(&mut aspace, boot_info, &mut flush);
         flush.flush().unwrap();
 
-        log::trace!("Kernel AddressSpace {aspace:?}");
+        tracing::trace!("Kernel AddressSpace {aspace:?}");
 
         Ok(Arc::new(Mutex::new(aspace)))
     })?;

kernel/src/task/mod.rs
@@ -727,7 +727,7 @@ where
     ///
     /// The caller has to ensure this cpu has exclusive mutable access to the tasks `stage` field (ie the
     /// future or output).
-    unsafe fn poll_inner(&self, mut cx: Context<'_>) -> Poll<()> {
+    pub unsafe fn poll_inner(&self, mut cx: Context<'_>) -> Poll<()> {
         let _span = self.span().enter();
 
         // Safety: ensured by caller

kernel/src/tests/mod.rs
@@ -7,16 +7,23 @@
 
 mod args;
 mod printer;
+mod smoke;
+mod spectest;
+mod wast;
 
+use crate::scheduler::scheduler;
 use crate::tests::args::Arguments;
 use crate::tests::printer::Printer;
 use crate::{arch, device_tree};
 use alloc::boxed::Box;
+use alloc::sync::Arc;
 use core::any::Any;
-use core::fmt::Write;
 use core::ptr::addr_of;
+use core::sync::atomic::{AtomicU64, Ordering};
 use core::{hint, slice};
-use ktest::{Test, TestInfo};
+use futures::FutureExt;
+use futures::future::try_join_all;
+use ktest::Test;
 
 /// The outcome of performing a single test.
 pub enum Outcome {
@@ -32,40 +39,22 @@ pub enum Outcome {
 pub struct Conclusion {
     /// Number of tests and benchmarks that were filtered out (either by the
     /// filter-in pattern or by `--skip` arguments).
-    pub num_filtered_out: u64,
+    pub num_filtered_out: AtomicU64,
 
     /// Number of passed tests.
-    pub num_passed: u64,
+    pub num_passed: AtomicU64,
 
     /// Number of failed tests and benchmarks.
-    pub num_failed: u64,
+    pub num_failed: AtomicU64,
 
     /// Number of ignored tests and benchmarks.
-    pub num_ignored: u64,
+    pub num_ignored: AtomicU64,
 
     /// Number of benchmarks that successfully ran.
-    pub num_measured: u64,
+    pub num_measured: AtomicU64,
 }
 
 impl Conclusion {
-    /// Returns an exit code that can be returned from `main` to signal
-    /// success/failure to the calling process.
-    pub fn exit_code(&self) -> i32 {
-        if self.has_failed() { 101 } else { 0 }
-    }
-
-    /// Returns whether there have been any failures.
-    pub fn has_failed(&self) -> bool {
-        self.num_failed > 0
-    }
-
-    /// Exits the application with an appropriate error code (0 if all tests
-    /// have passed, 101 if there have been failures). **This will not run any destructors.**
-    /// Consider using [`Self::exit_code`] instead for a proper program cleanup.
-    pub fn exit(&self) -> ! {
-        arch::exit(0);
-    }
-
     /// Exits the application with error code 101 if there were any failures.
     /// Otherwise, returns normally. **This will not run any destructors.**
     /// Consider using [`Self::exit_code`] instead for a proper program cleanup.
@@ -75,18 +64,23 @@ impl Conclusion {
         }
     }
 
+    /// Returns whether there have been any failures.
+    pub fn has_failed(&self) -> bool {
+        self.num_failed.load(Ordering::Acquire) > 0
+    }
+
     fn empty() -> Self {
         Self {
-            num_filtered_out: 0,
-            num_passed: 0,
-            num_failed: 0,
-            num_ignored: 0,
-            num_measured: 0,
+            num_filtered_out: AtomicU64::new(0),
+            num_passed: AtomicU64::new(0),
+            num_failed: AtomicU64::new(0),
+            num_ignored: AtomicU64::new(0),
+            num_measured: AtomicU64::new(0),
         }
     }
 }
 
-pub fn run_tests(write: &mut dyn Write, boot_info: &'static loader_api::BootInfo) -> Conclusion {
+pub async fn run_tests() -> Conclusion {
     let chosen = device_tree::device_tree().find_by_path("/chosen").unwrap();
     let args = if let Some(prop) = chosen.property("bootargs") {
         let str = prop.as_str().unwrap();
@@ -97,10 +91,8 @@ pub fn run_tests(write: &mut dyn Write, boot_info: &'static loader_api::BootInfo
 
     let tests = all_tests();
 
-    let mut conclusion = Conclusion::empty();
-
     // Create printer which is used for all output.
-    let mut printer = Printer::new(write, args.format);
+    let printer = Printer::new(args.format);
 
     // If `--list` is specified, just print the list and return.
     if args.list {
@@ -111,37 +103,44 @@ pub fn run_tests(write: &mut dyn Write, boot_info: &'static loader_api::BootInfo
     // Print number of tests
     printer.print_title(tests.len() as u64);
 
-    let mut handle_outcome = |outcome: Outcome, test: &TestInfo, printer: &mut Printer| {
-        printer.print_single_outcome(test, &outcome);
+    let printer = Arc::new(printer);
+    let conclusion = Arc::new(Conclusion::empty());
 
-        // Handle outcome
-        match outcome {
-            Outcome::Passed => conclusion.num_passed += 1,
-            Outcome::Failed(_reason) => {
-                conclusion.num_failed += 1;
-            }
-            Outcome::Ignored => conclusion.num_ignored += 1,
-        }
-    };
-
-    for test in tests {
-        // Print `test foo    ...`, run the test, then print the outcome in
-        // the same line.
-        printer.print_test(&test.info);
-        let outcome = if args.is_ignored(test) {
-            Outcome::Ignored
+    let tests = tests.iter().map(|test| {
+        if args.is_ignored(test) {
+            printer.print_test(&test.info);
+            conclusion.num_ignored.fetch_add(1, Ordering::Release);
+            futures::future::Either::Left(core::future::ready(Ok(())))
         } else {
-            match crate::panic::catch_unwind(|| (test.run)(boot_info)) {
-                Ok(_) => Outcome::Passed,
-                Err(err) => Outcome::Failed(err),
-            }
-        };
-        handle_outcome(outcome, &test.info, &mut printer);
-    }
+            let printer = printer.clone();
+            let conclusion = conclusion.clone();
+
+            let h = scheduler()
+                .spawn(async move {
+                    // Print `test foo    ...`, run the test, then print the outcome in
+                    // the same line.
+                    printer.print_test(&test.info);
+                    (test.run)().await;
+                })
+                .inspect(move |res| match res {
+                    Ok(_) => {
+                        conclusion.num_passed.fetch_add(1, Ordering::Release);
+                    }
+                    Err(_) => {
+                        conclusion.num_failed.fetch_add(1, Ordering::Release);
+                    }
+                });
+
+            futures::future::Either::Right(h)
+        }
+    });
+
+    // we handle test failures through `Conclusion` so it is safe to ignore the result here
+    let _ = try_join_all(tests).await;
 
     printer.print_summary(&conclusion);
 
-    conclusion
+    Arc::into_inner(conclusion).unwrap()
 }
 
 pub fn all_tests() -> &'static [Test] {

kernel/src/wasm/mod.rs
@@ -24,16 +24,14 @@ mod utils;
 mod values;
 mod vm;
 
-use crate::scheduler::scheduler;
-use crate::shell::Command;
 use crate::wasm::store::StoreOpaque;
 use crate::wasm::utils::{enum_accessors, owned_enum_accessors};
-use crate::wasm::vm::{ConstExprEvaluator, PlaceholderAllocatorDontUse};
-use alloc::boxed::Box;
+
 pub use engine::Engine;
-pub use func::{Func, TypedFunc};
+pub use func::Func;
 pub use global::Global;
 pub use instance::Instance;
+#[cfg(test)]
 pub use linker::Linker;
 pub use memory::Memory;
 pub use module::Module;
@@ -41,7 +39,10 @@ pub use store::Store;
 pub use table::Table;
 pub use tag::Tag;
 pub use trap::TrapKind;
-use wasmparser::Validator;
+#[cfg(test)]
+pub use values::Val;
+#[cfg(test)]
+pub use vm::{ConstExprEvaluator, PlaceholderAllocatorDontUse};
 
 /// The number of pages (for 32-bit modules) we can have before we run out of
 /// byte index space.
@@ -138,137 +139,4 @@ impl Extern {
         (Memory(Memory) into_memory e)
         (Global(Global) into_global e)
     }
-}
-
-pub const FIB_TEST: Command = Command::new("wasm-fib-test")
-    .with_help("run the WASM test payload")
-    .with_fn(|_| {
-        fib_test();
-        Ok(())
-    });
-
-pub const HOSTFUNC_TEST: Command = Command::new("wasm-hostfunc-test")
-    .with_help("run the WASM test payload")
-    .with_fn(|_| {
-        hostfunc_test();
-        Ok(())
-    });
-
-fn fib_test() {
-    use vm::ConstExprEvaluator;
-    use wasmparser::Validator;
-
-    let engine = Engine::default();
-    let mut validator = Validator::new();
-    let mut linker = Linker::new(&engine);
-    let mut store = Store::new(&engine, Box::new(PlaceholderAllocatorDontUse), ());
-    let mut const_eval = ConstExprEvaluator::default();
-
-    // instantiate & define the fib_cpp module
-    {
-        let module = Module::from_bytes(
-            &engine,
-            &mut validator,
-            include_bytes!("../../fib_cpp.wasm"),
-        )
-        .unwrap();
-
-        let instance = linker
-            .instantiate(&mut store, &mut const_eval, &module)
-            .unwrap();
-        instance.debug_vmctx(&store);
-
-        linker
-            .define_instance(&mut store, "fib_cpp", instance)
-            .unwrap();
-    }
-
-    // assert that we correctly parsed the fib export
-    assert!(linker.get(&mut store, "fib_cpp", "fib").unwrap().is_func());
-
-    // the module also exports its memory, assert that we correctly parsed that too
-    assert!(
-        linker
-            .get(&mut store, "fib_cpp", "memory")
-            .unwrap()
-            .is_memory()
-    );
-
-    // instantiate the test module
-    {
-        let module = Module::from_bytes(
-            &engine,
-            &mut validator,
-            include_bytes!("../../fib_test.wasm"),
-        )
-        .unwrap();
-
-        let instance = linker
-            .instantiate(&mut store, &mut const_eval, &module)
-            .unwrap();
-        instance.debug_vmctx(&store);
-
-        // `fib_test` should only have a single export
-        assert_eq!(instance.exports(&mut store).len(), 1);
-
-        let func: TypedFunc<(), ()> = instance
-            .get_func(&mut store, "fib_test")
-            .unwrap()
-            .typed(&store)
-            .unwrap();
-
-        scheduler().spawn(
-            crate::mem::KERNEL_ASPACE.get().unwrap().clone(),
-            async move {
-                func.call(&mut store, ()).unwrap();
-                tracing::info!("done");
-            },
-        );
-    }
-}
-
-fn hostfunc_test() {
-    let engine = Engine::default();
-    let mut validator = Validator::new();
-    let mut linker = Linker::new(&engine);
-    let mut store = Store::new(&engine, Box::new(PlaceholderAllocatorDontUse), ());
-    let mut const_eval = ConstExprEvaluator::default();
-
-    linker
-        .func_wrap("k23", "roundtrip_i64", |arg: u64| -> u64 {
-            tracing::debug!("Hello World from hostfunc!");
-            arg
-        })
-        .unwrap();
-
-    let module = Module::from_bytes(
-        &engine,
-        &mut validator,
-        include_bytes!("../../hostfunc_rs.wasm"),
-    )
-    .unwrap();
-
-    let instance = linker
-        .instantiate(&mut store, &mut const_eval, &module)
-        .unwrap();
-
-    instance.debug_vmctx(&store);
-
-    let func: TypedFunc<u64, u64> = instance
-        .get_func(&mut store, "roundtrip_i64")
-        .unwrap()
-        .typed(&store)
-        .unwrap();
-
-    scheduler().spawn(
-        crate::mem::KERNEL_ASPACE.get().unwrap().clone(),
-        async move {
-            let arg = 42;
-            let ret = func.call(&mut store, arg).unwrap();
-            assert_eq!(ret, arg);
-            tracing::info!("done");
-        },
-    );
-
-    tracing::info!("success!");
 }

kernel/src/wasm/store/mod.rs
@@ -38,7 +38,11 @@ pub(super) struct StoreInner<T> {
 }
 
 impl<T> Store<T> {
-    pub fn new(engine: &Engine, alloc: Box<dyn InstanceAllocator + Send + Sync>, data: T) -> Self {
+    pub fn new(
+        engine: &Engine,
+        alloc: &'static (dyn InstanceAllocator + Send + Sync),
+        data: T,
+    ) -> Self {
         let mut inner = Box::new(StoreInner {
             opaque: StoreOpaque {
                 engine: engine.clone(),
@@ -91,7 +95,7 @@ pub struct StoreOpaque {
     /// global type registry.
     engine: Engine,
     /// The instance allocator that manages all the runtime memory for Wasm instances.
-    alloc: Box<dyn InstanceAllocator + Send + Sync>,
+    alloc: &'static (dyn InstanceAllocator + Send + Sync),
     /// Data that is shared across all instances in this store such as stack limits, epoch pointer etc.
     /// This field is accessed by guest code
     vm_store_context: VMStoreContext,
@@ -133,8 +137,8 @@ impl StoreOpaque {
         &self.engine
     }
     #[inline]
-    pub(super) fn alloc_mut(&mut self) -> &mut (dyn InstanceAllocator + Send + Sync) {
-        self.alloc.as_mut()
+    pub(super) fn alloc_mut(&self) -> &(dyn InstanceAllocator + Send + Sync) {
+        self.alloc
     }
     #[inline]
     pub(super) fn vm_store_context(&self) -> &VMStoreContext {

kernel/src/wasm/vm/mod.rs
@@ -26,7 +26,9 @@ use crate::wasm::translate::TranslatedModule;
 pub use code_object::CodeObject;
 pub use const_eval::ConstExprEvaluator;
 pub use instance::{Instance, InstanceAndStore, InstanceHandle};
-pub use instance_alloc::{InstanceAllocator, PlaceholderAllocatorDontUse};
+pub use instance_alloc::InstanceAllocator;
+#[cfg(test)]
+pub use instance_alloc::PlaceholderAllocatorDontUse;
 pub use memory::Memory;
 pub use mmap_vec::MmapVec;
 pub use provenance::VmPtr;

commit b9471002809cdf6a5dd04620912df189de5a3ff5

fix: regression of IRQ wakeup
This fixes a regression in the IRQ handling introduced by 429b5c4556416ec79e0ad1c86a3423b7c58d1d72 where cores would treat all IRQs as spurious wakeups

kernel/src/arch/riscv64/trap_handler.rs
@@ -309,6 +309,7 @@ extern "C-unwind" fn default_trap_handler(
             Trap::Interrupt(Interrupt::SupervisorExternal) => with_cpu(|cpu| {
                 let mut plic = cpu.plic.borrow_mut();
                 irq::trigger_irq(plic.deref_mut());
+                scheduler().idle.notify_self();
             }),
             Trap::Exception(
                 Exception::LoadPageFault

kernel/src/irq.rs
@@ -42,12 +42,13 @@ pub fn trigger_irq(irq_ctl: &mut dyn InterruptController) {
         return;
     };
 
+    // acknowledge the interrupt as fast as possible
+    irq_ctl.irq_complete(claim);
+
     let queues = QUEUES.read();
     if let Some(queue) = queues.get(&claim.as_u32()) {
         queue.wake_all();
     }
-
-    irq_ctl.irq_complete(claim);
 }
 
 pub async fn next_event(irq_num: u32) -> Result<(), sync::Closed> {

kernel/src/scheduler/idle.rs
@@ -9,6 +9,7 @@ use crate::scheduler::park::{ParkToken, UnparkToken};
 use alloc::vec::Vec;
 use core::sync::atomic::{AtomicUsize, Ordering};
 use spin::Mutex;
+use crate::CPUID;
 
 pub struct Idle {
     /// Number of searching workers
@@ -117,6 +118,16 @@ impl Idle {
         while let Some(worker) = self.sleepers.lock().pop() {
             worker.unpark();
         }
+    }
+
+    // FIXME this implementation is baaaad, especially because its run from a trap handler
+    //  it would be much better to use cpu_local storage for this
+    pub fn notify_self(&self) {
+        let cpuid = CPUID.get();
+        let mut sleepers = self.sleepers.lock();
+        let index = sleepers.iter().position(|worker| worker.cpuid() == cpuid).unwrap();
+        let worker = sleepers.remove(index);
+        worker.unpark();
     }
 }
 

kernel/src/scheduler/mod.rs
@@ -122,7 +122,7 @@ pub struct Scheduler {
     /// All tasks currently scheduled on this runtime
     owned: OwnedTasks,
     /// Coordinates idle workers
-    idle: Idle,
+    pub idle: Idle,
     /// Signal to workers that they should be shutting down.
     shutdown: AtomicBool,
     /// Spin barrier used to synchronize shutdown between workers,