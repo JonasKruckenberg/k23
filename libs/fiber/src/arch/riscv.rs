a2a7a8d947ffaff4a38d7fb0a332738b370b8a27
Jonas Kruckenberg <iterpre@protonmail.com>
2025-05-28 22:45:59 +0200

    Update riscv.rs

--- libs/fiber/src/arch/riscv.rs
+++ libs/fiber/src/arch/riscv.rs
@@ -1,10 +1,3 @@
-// Copyright 2025 Jonas Kruckenberg
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
-
 //! ## Stack layout
 //!
 //! Here is what the layout of the stack looks like when a coroutine is
@@ -15,7 +8,7 @@
 //! | Initial func |
 //! +--------------+
 //! | Parent link  |
-//! +--------------+
+a//! +--------------+
 //! +--------------+
 //! |              |
 //! ~ Fiber-local  ~
