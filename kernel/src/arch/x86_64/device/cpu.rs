2a77b1c1219a465a7487d8b5353b362f4a0f3713
bubblepipe <bubblepipe42@gmail.com>
2025-06-16 13:37:17 +0800

    add clock to cpu

--- kernel/src/arch/x86_64/device/cpu.rs
+++ kernel/src/arch/x86_64/device/cpu.rs
@@ -10,11 +10,18 @@ use crate::device_tree::DeviceTree;
 #[derive(Debug)]
 pub struct Cpu {
     pub id: usize,
+    pub clock: Clock,
 }
 
 impl Cpu {
     pub fn new(_devtree: &DeviceTree, cpuid: usize) -> crate::Result<Self> {
+        let tick_duration = Duration::from_nanos(NANOS_PER_SEC / timebase_frequency);
+        let clock = Clock::new(tick_duration, || Ticks(riscv::register::time::read64()));
+
         // TODO: Initialize x86_64 CPU from device tree or ACPI tables
-        Ok(Self { id: cpuid })
+        Ok(Self {
+            id: cpuid,
+            clock: clock,
+        })
     }
 }
