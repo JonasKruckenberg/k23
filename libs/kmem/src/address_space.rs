ecfa5b2305d5f04599a5ef9325e180e57fc61d79
Jonas Kruckenberg <iterpre@protonmail.com>
2025-09-22 21:11:20 +0200

    wip(kmem): implement gap search
    This change implements a gap-searching algorithm, loosely based on Zircon's (https://cs.opensource.google/fuchsia/fuchsia/+/main:zircon/kernel/vm/include/vm/vm_address_region.h;drc=7b51c3ec3aa33ffba6dde43ca53a2aab01983e00;bpv=1;bpt=1;l=551).
    
    The implemented algorithm maintains metadata required for binary search at each AddressSpaceRegion and then performs a search over the gaps of the tree.
    It naturally implements ASLR by skipping gaps according to a randomly chosen index. The PRNG used for this is ChaCha20 implemented by the `rand` ecosystem. This change also adds a rundimentary statistical test to ensure the spot allocation indeed allocates with enough entropy.

--- libs/kmem/src/address_space.rs
+++ libs/kmem/src/address_space.rs
@@ -8,28 +8,32 @@
 mod region;
 
 use core::alloc::Layout;
+use core::ops::{ControlFlow, Range};
 use core::ptr::NonNull;
 
+use rand::Rng;
+use rand::distr::Uniform;
+use rand_chacha::ChaCha20Rng;
 use wavltree::WAVLTree;
 
-use crate::AccessRules;
 use crate::address_space::region::AddressSpaceRegion;
+use crate::{AccessRules, AddressRangeExt, VirtualAddress};
 
 pub struct AddressSpace {
     regions: WAVLTree<AddressSpaceRegion>,
+    max_range: Range<VirtualAddress>,
+    rng: Option<ChaCha20Rng>,
 }
 
 // ===== impl AddressSpace =====
 
-impl Default for AddressSpace {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 impl AddressSpace {
-    pub const fn new() -> Self {
-        todo!()
+    pub const fn new(max_range: Range<VirtualAddress>, rng: Option<ChaCha20Rng>) -> Self {
+        Self {
+            regions: WAVLTree::new(),
+            max_range,
+            rng,
+        }
     }
 
     /// Attempts to reserve a region of virtual memory.
@@ -288,5 +292,378 @@ impl AddressSpace {
 
     pub fn assert_valid(&self, ctx: &str) {
         self.regions.assert_valid(ctx);
+    }
+
+    /// Find a spot in the address space that satisfies the given `layout` requirements.
+    ///
+    /// If a spot suitable for holding data described by `layout` is found, the base address of the
+    /// address range is returned in `Some`. The returned address is already correct aligned to
+    /// `layout.align()`.
+    ///
+    /// Returns `None` if no suitable spot was found. This *does not* mean there are no more gaps in
+    /// the address space just that the *combination* of `layout.size()` and `layout.align()` cannot
+    /// be satisfied *at the moment*. Calls to this method will a different size, alignment, or at a
+    /// different time might still succeed.
+    #[cfg_attr(not(test), expect(unused, reason = "used by tests and later changes"))]
+    fn find_spot_for(&mut self, layout: Layout) -> Option<VirtualAddress> {
+        // The algorithm we use here - loosely based on Zircon's (Fuchsia's) implementation - is
+        // guaranteed to find a spot (if any even exist) with max 2 attempts. Additionally, it works
+        // elegantly *with* AND *without* ASLR, picking a random spot or the lowest free spot respectively.
+        // Here is how it works:
+        // 1. We set up two counters: (see the GapVisitor)
+        //    - `candidate_spot_count` which we initialize to zero
+        //    - `target_index` which we either set to a random value between 0..<the maximum number of
+        //       possible addresses in the address space> if ASLR is requested OR to zero otherwise.
+        // 2. We then iterate over all `AddressSpaceRegion`s from lowest to highest looking at the
+        //    gaps between regions. We count the number of addresses in each gap that satisfy the
+        //    requested `Layout`s size and alignment and add that to the `candidate_spot_count`.
+        //    IF the number of spots in the gap is greater than our chosen target index, we pick the
+        //    spot at the target index and finish. ELSE we *decrement* the target index by the number
+        //    of spots and continue to the next gap.
+        // 3. After we have processed all the gaps, we have EITHER found a suitable spot OR our original
+        //    guess for `target_index` was too big, in which case we need to retry.
+        // 4. When retrying we iterate over all `AddressSpaceRegion`s *again*, but this time we know
+        //    the *actual* number of possible spots in the address space since we just counted them
+        //    during the first attempt. We initialize `target_index` to `0..candidate_spot_count`
+        //    which is guaranteed to return us a spot.
+        //    IF `candidate_spot_count` is ZERO after the first attempt, there is no point in
+        //    retrying since we cannot fulfill the requested layout.
+        //
+        // Note that in practice, we use a binary tree to keep track of regions, and we use binary search
+        // to optimize the search for a suitable gap instead of linear iteration.
+
+        let layout = layout.pad_to_align();
+
+        // First attempt: guess a random target index
+        let max_candidate_spots = self.max_range.size();
+
+        let target_index: usize = self
+            .rng
+            .as_mut()
+            .map(|prng| prng.sample(Uniform::new(0, max_candidate_spots).unwrap()))
+            .unwrap_or_default();
+
+        // First attempt: visit the binary search tree to find a gap
+        let mut v = GapVisitor::new(layout, target_index);
+        self.visit_gaps(&mut v);
+
+        // if we found a spot already we're done
+        if let Some(chosen) = v.chosen {
+            return Some(chosen);
+        }
+
+        // otherwise, Second attempt: we need to retry with the correct candidate spot count
+        // but if we counted no suitable candidate spots during the first attempt, we cannot fulfill
+        // the request.
+        if v.candidate_spots == 0 {
+            return None;
+        }
+
+        // Second attempt: pick a new target_index that's actually fulfillable
+        let target_index: usize = self
+            .rng
+            .as_mut()
+            .map(|prng| prng.sample(Uniform::new(0, v.candidate_spots).unwrap()))
+            .unwrap_or_default();
+
+        // Second attempt: visit the binary search tree to find a gap
+        let mut v = GapVisitor::new(layout, target_index);
+        self.visit_gaps(&mut v);
+
+        let chosen = v
+            .chosen
+            .expect("There must be a chosen spot after the first attempt. This is a bug!");
+
+        Some(chosen)
+    }
+
+    /// Visit all gaps (address ranges not covered by an [`AddressSpaceRegion`]) in this address space
+    /// from lowest to highest addresses.
+    fn visit_gaps(&self, v: &mut GapVisitor) {
+        let Some(root) = self.regions.root().get() else {
+            // if the tree is empty, we treat the entire max_range as the gap
+            // note that we do not care about the returned ControlFlow, as there is nothing else we
+            // could try to find a spot anyway
+            let _ = v.visit(self.max_range.clone());
+
+            return;
+        };
+
+        // see if there is a suitable gap between BEFORE the first address space region
+        if v.visit(self.max_range.start..root.subtree_range().start)
+            .is_break()
+        {
+            return;
+        }
+
+        // now comes the main part of the search. we start at the WAVLTree root node and do a
+        // binary search for a suitable gap. We use special metadata on each `AddressSpaceRegion`
+        // to speed up this search. See `AddressSpaceRegion`  for details on how this works.
+
+        let mut maybe_current = self.regions.root().get();
+        let mut already_visited = VirtualAddress::MIN;
+
+        while let Some(current) = maybe_current {
+            // If there is no suitable gap in this entire
+            if current.suitable_gap_in_subtree(v.layout()) {
+                // First, look at the left subtree
+                if let Some(left) = current.left_child() {
+                    if left.suitable_gap_in_subtree(v.layout())
+                        && left.subtree_range().end > already_visited
+                    {
+                        maybe_current = Some(left);
+                        continue;
+                    }
+
+                    if v.visit(left.subtree_range().end..current.range().start)
+                        .is_break()
+                    {
+                        return;
+                    }
+                }
+
+                if let Some(right) = current.right_child() {
+                    if v.visit(current.range().end..right.subtree_range().start)
+                        .is_break()
+                    {
+                        return;
+                    }
+
+                    if right.suitable_gap_in_subtree(v.layout())
+                        && right.subtree_range().end > already_visited
+                    {
+                        maybe_current = Some(right);
+                        continue;
+                    }
+                }
+            }
+
+            already_visited = current.subtree_range().end;
+            maybe_current = current.parent();
+        }
+
+        // see if there is a suitable gap between AFTER the last address space region
+        // NB: regardless of the function return we reached the end of this function anyway, there
+        // is nothing else we can do. It's therefore fine to ignore the result.
+        let _ = v.visit(root.subtree_range().end..self.max_range.end);
+    }
+}
+
+struct GapVisitor {
+    layout: Layout,
+    target_index: usize,
+    candidate_spots: usize,
+    chosen: Option<VirtualAddress>,
+}
+
+impl GapVisitor {
+    fn new(layout: Layout, target_index: usize) -> Self {
+        Self {
+            layout,
+            target_index,
+            candidate_spots: 0,
+            chosen: None,
+        }
+    }
+
+    /// Returns the [`Layout`] that we are allocating for.
+    pub fn layout(&self) -> Layout {
+        self.layout
+    }
+
+    /// Visitor callback for a gap in the tree. This method will return `ControlFlow::Continue`
+    /// to indicate the caller should continue traversing the tree and `ControlFlow::Break` to stop.
+    pub fn visit(&mut self, gap: Range<VirtualAddress>) -> ControlFlow<()> {
+        // if we have already chosen a spot, signal the caller to stop
+        if self.chosen.is_some() {
+            return ControlFlow::Break(());
+        }
+
+        let aligned_gap = gap.checked_align_in(self.layout.align()).unwrap();
+
+        let spot_count = self.spots_in_range(&aligned_gap);
+
+        self.candidate_spots += spot_count;
+
+        if self.target_index < spot_count {
+            self.chosen = Some(
+                aligned_gap
+                    .start
+                    .checked_add(self.target_index << self.layout.align().ilog2())
+                    .unwrap(),
+            );
+
+            ControlFlow::Break(())
+        } else {
+            self.target_index -= spot_count;
+
+            ControlFlow::Continue(())
+        }
+    }
+
+    /// Returns the number of spots in the given range that satisfy the layout we require
+    fn spots_in_range(&self, range: &Range<VirtualAddress>) -> usize {
+        debug_assert!(
+            range.start.is_aligned_to(self.layout.align())
+                && range.end.is_aligned_to(self.layout.align())
+        );
+
+        // ranges passed in here can become empty for a number of reasons (aligning might produce ranges
+        // where end > start, or the range might be empty to begin with) in either case an empty
+        // range means no spots are available
+        if range.is_empty() {
+            return 0;
+        }
+
+        let range_size = range.size();
+        if range_size >= self.layout.size() {
+            range_size / self.layout.size()
+        } else {
+            0
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::boxed::Box;
+    use alloc::vec::Vec;
+
+    use rand::SeedableRng;
+
+    use super::*;
+
+    #[test]
+    fn find_spot_for_no_aslr() {
+        // ===== find a spot *after* the regions =====
+        let mut aspace = AddressSpace::new(VirtualAddress::MIN..VirtualAddress::MAX, None);
+
+        aspace.regions.insert(Box::pin(AddressSpaceRegion::new(
+            VirtualAddress::new(0),
+            AccessRules::new().with(AccessRules::READ, true),
+            Layout::from_size_align(4 * 4096, 4096).unwrap(),
+        )));
+
+        aspace.regions.insert(Box::pin(AddressSpaceRegion::new(
+            VirtualAddress::new(4 * 4096),
+            AccessRules::new().with(AccessRules::READ, true),
+            Layout::from_size_align(4 * 4096, 4096).unwrap(),
+        )));
+
+        // we know the following: NO gap to the left, NO gap between the regions and A BIG gap after
+        // we therefore expect even the smallest layout to be placed AFTER 9 * 4096
+        let spot = aspace
+            .find_spot_for(Layout::from_size_align(4096, 4096).unwrap())
+            .unwrap();
+
+        assert_eq!(spot, VirtualAddress::new(8 * 4096));
+
+        // ===== find a spot *between* the regions =====
+
+        let mut aspace = AddressSpace::new(VirtualAddress::MIN..VirtualAddress::MAX, None);
+
+        aspace.regions.insert(Box::pin(AddressSpaceRegion::new(
+            VirtualAddress::new(0),
+            AccessRules::new().with(AccessRules::READ, true),
+            Layout::from_size_align(4 * 4096, 4096).unwrap(),
+        )));
+
+        aspace.regions.insert(Box::pin(AddressSpaceRegion::new(
+            VirtualAddress::new(5 * 4096),
+            AccessRules::new().with(AccessRules::READ, true),
+            Layout::from_size_align(4 * 4096, 4096).unwrap(),
+        )));
+
+        // we know the following: NO gap to the left, ONE page gap between the regions and A BIG gap after
+        let spot = aspace
+            .find_spot_for(Layout::from_size_align(4096, 4096).unwrap())
+            .unwrap();
+
+        assert_eq!(spot, VirtualAddress::new(4 * 4096));
+
+        // ===== find a spot *before* the regions =====
+
+        let mut aspace = AddressSpace::new(VirtualAddress::MIN..VirtualAddress::MAX, None);
+
+        aspace.regions.insert(Box::pin(AddressSpaceRegion::new(
+            VirtualAddress::new(4 * 4096),
+            AccessRules::new().with(AccessRules::READ, true),
+            Layout::from_size_align(4 * 4096, 4096).unwrap(),
+        )));
+
+        aspace.regions.insert(Box::pin(AddressSpaceRegion::new(
+            VirtualAddress::new(8 * 4096),
+            AccessRules::new().with(AccessRules::READ, true),
+            Layout::from_size_align(4 * 4096, 4096).unwrap(),
+        )));
+
+        // we know the following: NO gap to the left, ONE page gap between the regions and A BIG gap after
+        let spot = aspace
+            .find_spot_for(Layout::from_size_align(4096, 4096).unwrap())
+            .unwrap();
+
+        assert_eq!(spot, VirtualAddress::new(0));
+    }
+
+    #[test]
+    fn find_spot_aslr_entropy() {
+        let layout = Layout::from_size_align(4096, 4096).unwrap();
+
+        let mut aspace = AddressSpace::new(
+            VirtualAddress::MIN..VirtualAddress::MAX,
+            Some(ChaCha20Rng::from_os_rng()),
+        );
+
+        // first we fill up the address space with 100 randomly placed regions just so
+        // we are not just testing the entropy of the RNG
+        for _ in 0..100 {
+            let spot = aspace.find_spot_for(layout).unwrap();
+            aspace.regions.insert(Box::pin(AddressSpaceRegion::new(
+                spot,
+                AccessRules::new().with(AccessRules::READ, true),
+                layout,
+            )));
+        }
+
+        let mut rng = ChaCha20Rng::from_os_rng();
+
+        // then we sample the algorithm 500 times, adding the chosen spots to the test buffer
+        let mut data = Vec::new();
+        for _ in 0..5000 {
+            let spot = aspace.find_spot_for(layout).unwrap();
+
+            // because all spots are page-aligned we need to generate the lower 12 bits of randomness for
+            // our statistical test to pass
+            let spot = spot.get() | rng.sample(Uniform::new(0, 4096).unwrap());
+
+            data.push(VirtualAddress::new(spot));
+        }
+
+        // finally we run the Frequency (Monobit) Test over the collected data so see if
+        // we get proper distribution
+        let (passed, pval) = frequency_test(&data);
+
+        assert!(passed, "test returned P-value of {pval} (expected >= 0.01)");
+    }
+
+    /// Implements the Frequency (Monobit) Test from NIST
+    /// "STATISTICAL TEST SUITE FOR RANDOM AND PSEUDORANDOM NUMBER GENERATORS FOR CRYPTOGRAPHIC APPLICATIONS"
+    /// (<https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-22r1a.pdf>)
+    ///
+    ///
+    pub fn frequency_test(spots: &[VirtualAddress]) -> (bool, f64) {
+        const TEST_THRESHOLD: f64 = 0.01;
+
+        let nbits = spots.len() * VirtualAddress::BITS as usize;
+        let ones: usize = spots
+            .into_iter()
+            .map(|spot| spot.get().count_ones() as usize)
+            .sum();
+
+        let sn = 2 * ones as isize - nbits as isize;
+        let sobs = sn.abs() as f64 / (nbits as f64).sqrt();
+        let p = libm::erfc(sobs / 2.0_f64.sqrt());
+
+        (p >= TEST_THRESHOLD, p)
     }
 }
--- libs/kmem/src/address_space/region.rs
+++ libs/kmem/src/address_space/region.rs
@@ -11,6 +11,7 @@ use core::mem::offset_of;
 use core::ops::Range;
 use core::pin::Pin;
 use core::ptr::NonNull;
+use core::{cmp, mem};
 
 use pin_project::pin_project;
 
@@ -32,6 +33,129 @@ pub struct AddressSpaceRegion {
     /// Links to other regions in the WAVL tree
     #[pin]
     links: wavltree::Links<AddressSpaceRegion>,
+}
+
+impl AddressSpaceRegion {
+    #[cfg_attr(not(test), allow(unused, reason = "used by tests and later changes"))]
+    pub const fn new(
+        spot: VirtualAddress,
+        access_rules: AccessRules,
+        #[cfg(debug_assertions)] layout: Layout,
+    ) -> Self {
+        Self {
+            range: spot..spot.checked_add(layout.size()).unwrap(),
+            access_rules,
+            #[cfg(debug_assertions)]
+            layout,
+
+            max_gap: 0,
+            subtree_range: spot..spot.checked_add(layout.size()).unwrap(),
+            links: wavltree::Links::new(),
+        }
+    }
+
+    pub const fn range(&self) -> &Range<VirtualAddress> {
+        &self.range
+    }
+
+    pub const fn subtree_range(&self) -> &Range<VirtualAddress> {
+        &self.subtree_range
+    }
+
+    /// Returns `true` if this nodes subtree contains a gap suitable for the given `layout`, used
+    /// during gap-searching.
+    pub fn suitable_gap_in_subtree(&self, layout: Layout) -> bool {
+        // we need the layout to be padded to alignment
+        debug_assert!(layout.size().is_multiple_of(layout.align()));
+
+        self.max_gap >= layout.size()
+    }
+
+    /// Returns the left child node in the search tree of regions, used during gap-searching.
+    pub fn left_child(&self) -> Option<&Self> {
+        // Safety: we have to trust the intrusive tree implementation here
+        Some(unsafe { self.links.left()?.as_ref() })
+    }
+
+    /// Returns the right child node in the search tree of regions, used during gap-searching.
+    pub fn right_child(&self) -> Option<&Self> {
+        // Safety: we have to trust the intrusive tree implementation here
+        Some(unsafe { self.links.right()?.as_ref() })
+    }
+
+    /// Returns the parent node in the search tree of regions, used during gap-searching.
+    pub fn parent(&self) -> Option<&Self> {
+        // Safety: we have to trust the intrusive tree implementation here
+        Some(unsafe { self.links.parent()?.as_ref() })
+    }
+
+    /// Update the gap search metadata of this region. This method is called in the [`wavltree::Linked`]
+    /// implementation below after each tree mutation that impacted this node or its subtree in some way
+    /// (insertion, rotation, deletion).
+    ///
+    /// Returns `true` if this nodes metadata changed.
+    fn update_gap_metadata(
+        mut node: NonNull<Self>,
+        left: Option<NonNull<Self>>,
+        right: Option<NonNull<Self>>,
+    ) -> bool {
+        fn gap(left_last_byte: VirtualAddress, right_first_byte: VirtualAddress) -> usize {
+            right_first_byte
+                .checked_sub_addr(left_last_byte)
+                .unwrap_or_default() // TODO use saturating_sub_addr
+        }
+
+        // Safety: we have to trust the intrusive tree implementation
+        let node = unsafe { node.as_mut() };
+        let mut left_max_gap = 0;
+        let mut right_max_gap = 0;
+
+        // recalculate the subtree_range start
+        let old_subtree_range_start = if let Some(left) = left {
+            // Safety: we have to trust the intrusive tree implementation
+            let left = unsafe { left.as_ref() };
+            let left_gap = gap(left.subtree_range.end, node.range.start);
+            left_max_gap = cmp::max(left_gap, left.max_gap);
+            mem::replace(&mut node.subtree_range.start, left.subtree_range.start)
+        } else {
+            mem::replace(&mut node.subtree_range.start, node.range.start)
+        };
+
+        // recalculate the subtree range end
+        let old_subtree_range_end = if let Some(right) = right {
+            // Safety: we have to trust the intrusive tree implementation
+            let right = unsafe { right.as_ref() };
+            let right_gap = gap(node.range.end, right.subtree_range.start);
+            right_max_gap = cmp::max(right_gap, right.max_gap);
+            mem::replace(&mut node.subtree_range.end, right.subtree_range.end)
+        } else {
+            mem::replace(&mut node.subtree_range.end, node.range.end)
+        };
+
+        // recalculate the map_gap
+        let old_max_gap = mem::replace(&mut node.max_gap, cmp::max(left_max_gap, right_max_gap));
+
+        old_max_gap != node.max_gap
+            || old_subtree_range_start != node.subtree_range.start
+            || old_subtree_range_end != node.subtree_range.end
+    }
+
+    // Propagate metadata updates to this regions parent in the search tree. If we had to update
+    // our metadata the parent must update its metadata too.
+    fn propagate_update_to_parent(mut maybe_node: Option<NonNull<Self>>) {
+        while let Some(node) = maybe_node {
+            // Safety: we have to trust the intrusive tree implementation
+            let links = unsafe { &node.as_ref().links };
+            let changed = Self::update_gap_metadata(node, links.left(), links.right());
+
+            // if the metadata didn't actually change, we don't need to recalculate parents
+            if !changed {
+                return;
+            }
+
+            maybe_node = links.parent();
+        }
+    }
 }
 
 // Safety: the pinning and !Unpin requirements are enforced by the `#[pin_project(!Unpin)]` attribute
@@ -68,5 +192,157 @@ unsafe impl wavltree::Linked for AddressSpaceRegion {
 
     fn get_key(&self) -> &Self::Key {
         &self.range.start
+    }
+
+    fn after_insert(self: Pin<&mut Self>) {
+        debug_assert_eq!(self.subtree_range.start, self.range.start);
+        debug_assert_eq!(self.subtree_range.end, self.range.end);
+        debug_assert_eq!(self.max_gap, 0);
+        Self::propagate_update_to_parent(self.links.parent());
+    }
+
+    fn after_remove(self: Pin<&mut Self>, parent: Option<NonNull<Self>>) {
+        Self::propagate_update_to_parent(parent);
+    }
+
+    fn after_rotate(
+        self: Pin<&mut Self>,
+        parent: NonNull<Self>,
+        sibling: Option<NonNull<Self>>,
+        lr_child: Option<NonNull<Self>>,
+        side: wavltree::Side,
+    ) {
+        let this = self.project();
+        // Safety: caller ensures ptr is valid
+        let _parent = unsafe { parent.as_ref() };
+
+        this.subtree_range.start = _parent.subtree_range.start;
+        this.subtree_range.end = _parent.subtree_range.end;
+        *this.max_gap = _parent.max_gap;
+
+        if side == wavltree::Side::Left {
+            Self::update_gap_metadata(parent, sibling, lr_child);
+        } else {
+            Self::update_gap_metadata(parent, lr_child, sibling);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use core::alloc::Layout;
+
+    use wavltree::WAVLTree;
+
+    use super::*;
+    use crate::{AccessRules, VirtualAddress};
+
+    #[test]
+    fn region_insert_higher() {
+        let mut tree: WAVLTree<AddressSpaceRegion> = WAVLTree::new();
+
+        tree.insert(Box::pin(AddressSpaceRegion::new(
+            VirtualAddress::new(0),
+            AccessRules::new().with(AccessRules::READ, true),
+            Layout::from_size_align(4 * 4096, 4096).unwrap(),
+        )));
+
+        tree.insert(Box::pin(AddressSpaceRegion::new(
+            VirtualAddress::new(5 * 4096),
+            AccessRules::new().with(AccessRules::READ, true),
+            Layout::from_size_align(4 * 4096, 4096).unwrap(),
+        )));
+
+        let a = tree.find(&VirtualAddress::new(0)).get().unwrap();
+        let b = tree.find(&VirtualAddress::new(5 * 4096)).get().unwrap();
+
+        // we expect the *first* region to be the parent and therefore hold the big range
+        assert_eq!(
+            a.subtree_range,
+            VirtualAddress::new(0)..VirtualAddress::new(9 * 4096)
+        );
+        assert_eq!(a.max_gap, 4096);
+
+        // the *second* subtree should just hold its own range
+        assert_eq!(
+            b.subtree_range,
+            VirtualAddress::new(5 * 4096)..VirtualAddress::new(9 * 4096)
+        );
+        assert_eq!(b.range, b.subtree_range);
+        assert_eq!(b.max_gap, 0);
+    }
+
+    #[test]
+    fn region_insert_lower() {
+        let mut tree: WAVLTree<AddressSpaceRegion> = WAVLTree::new();
+
+        tree.insert(Box::pin(AddressSpaceRegion::new(
+            VirtualAddress::new(5 * 4096),
+            AccessRules::new().with(AccessRules::READ, true),
+            Layout::from_size_align(4 * 4096, 4096).unwrap(),
+        )));
+        tree.insert(Box::pin(AddressSpaceRegion::new(
+            VirtualAddress::new(0),
+            AccessRules::new().with(AccessRules::READ, true),
+            Layout::from_size_align(4 * 4096, 4096).unwrap(),
+        )));
+
+        let a = tree.find(&VirtualAddress::new(0)).get().unwrap();
+        let b = tree.find(&VirtualAddress::new(5 * 4096)).get().unwrap();
+
+        // the *second* subtree should just hold its own range
+        assert_eq!(
+            b.subtree_range,
+            VirtualAddress::new(0)..VirtualAddress::new(9 * 4096)
+        );
+        assert_eq!(b.max_gap, 4096);
+
+        // we expect the *first* region to be the parent and therefore hold the big range
+        assert_eq!(
+            a.subtree_range,
+            VirtualAddress::new(0)..VirtualAddress::new(4 * 4096)
+        );
+        assert_eq!(a.range, a.subtree_range);
+        assert_eq!(a.max_gap, 0);
+    }
+
+    #[test]
+    fn region_updates_after_rotates() {
+        let mut tree: WAVLTree<AddressSpaceRegion> = WAVLTree::new();
+        let mut addr = VirtualAddress::new(0);
+
+        for _ in 0..10 {
+            tree.insert(Box::pin(AddressSpaceRegion::new(
+                addr,
+                AccessRules::new().with(AccessRules::READ, true),
+                Layout::from_size_align(4096, 4096).unwrap(),
+            )));
+            addr = addr.checked_add(11 * 4096).unwrap();
+        }
+
+        for region in tree.iter() {
+            if region.left_child().is_some() || region.right_child().is_some() {
+                // all gaps are the same size (10 pages)
+                assert_eq!(region.max_gap, 10 * 4096);
+            } else {
+                // regions without children should not have a max_gap
+                assert_eq!(region.max_gap, 0);
+            }
+
+            assert_eq!(calculate_subtree_range(region), region.subtree_range)
+        }
+    }
+
+    fn calculate_subtree_range(region: &AddressSpaceRegion) -> Range<VirtualAddress> {
+        let mut range = region.range.clone();
+
+        if let Some(left) = region.left_child().map(calculate_subtree_range) {
+            range.start = cmp::min(range.start, left.start);
+        }
+        if let Some(right) = region.right_child().map(calculate_subtree_range) {
+            range.end = cmp::max(range.end, right.end);
+        }
+
+        range
     }
 }
