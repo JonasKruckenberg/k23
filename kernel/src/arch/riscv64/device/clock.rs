JonasKruckenberg/k23

commit 1656fcf50a1d2d35569ef5710ebccde45d48f654

refactor: simplify `kasync` (#487)

kernel/src/arch/riscv64/device/clock.rs
@@ -1,0 +1,73 @@
+// Copyright 2025. Jonas Kruckenberg
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+use crate::device_tree::Device;
+use core::ptr;
+use core::time::Duration;
+use kasync::time::{Clock, NANOS_PER_SEC, RawClock, RawClockVTable};
+use riscv::sbi;
+
+static CLOCK_VTABLE: RawClockVTable =
+    RawClockVTable::new(clone_raw, now_raw, schedule_wakeup_raw, drop_raw);
+
+unsafe fn clone_raw(ptr: *const ()) -> RawClock {
+    tracing::trace!(
+        clock.addr = ?ptr,
+        "RISCV CLOCK::clone_raw"
+    );
+    debug_assert!(ptr.is_null());
+    RawClock::new(ptr, &CLOCK_VTABLE)
+}
+
+unsafe fn now_raw(_ptr: *const ()) -> u64 {
+    tracing::trace!(
+        clock.addr = ?_ptr,
+        "RISCV CLOCK::now_raw"
+    );
+    debug_assert!(_ptr.is_null());
+
+    riscv::register::time::read64()
+}
+
+unsafe fn schedule_wakeup_raw(_ptr: *const (), at: u64) {
+    tracing::trace!(
+        clock.addr = ?_ptr,
+        "RISCV CLOCK::schedule_wakeup_raw"
+    );
+    debug_assert!(_ptr.is_null());
+
+    sbi::time::set_timer(at).unwrap();
+}
+
+unsafe fn drop_raw(_ptr: *const ()) {
+    tracing::trace!(
+        clock.addr = ?_ptr,
+        "RISCV CLOCK::drop_raw"
+    );
+    debug_assert!(_ptr.is_null());
+}
+
+pub fn new(cpu_node: &Device) -> crate::Result<Clock> {
+    let timebase_frequency = cpu_node
+        .property("timebase-frequency")
+        .or_else(|| cpu_node.parent().unwrap().property("timebase-frequency"))
+        .unwrap()
+        .as_u64()?;
+
+    let tick_duration = Duration::from_nanos(NANOS_PER_SEC / timebase_frequency);
+
+    // Safety: HAHA, actually this ISN'T SAFE! Technically both `now_raw` and `now_schedule_wakeup`
+    // access HART-local registers for the time & timeout, so in the scenario that different HARTs in
+    // the system disagree about what time it is, we're in real big trouble. Unfortunately, I can't
+    // think of a great way to solve this that doesn't involve incrementing a global on a timer or
+    // choosing one hart that maintains & drives timers.
+    // *Fortunately* this situation is probably quite rare, so we're going to ignore this for now.
+    // FIXME: <https://github.com/JonasKruckenberg/k23/issues/490>
+    let clock = unsafe { Clock::new(tick_duration, ptr::null(), &CLOCK_VTABLE) };
+
+    Ok(clock.named("RISCV CLOCK"))
+}

libs/kasync/src/time/clock.rs
@@ -1,41 +1,109 @@
-// Copyright 2025 Jonas Kruckenberg
+// Copyright 2025. Jonas Kruckenberg
 //
 // Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use crate::time::{Instant, NANOS_PER_SEC, TimeError};
+use crate::time::max_duration;
 use core::fmt;
 use core::time::Duration;
 
-/// [`Clock`] ticks are always counted by a 64-bit unsigned integer.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Ticks(pub u64);
-
-/// A hardware clock definition.
-///
-/// A `Clock` consists of a function that returns the hardware clock's current
-/// timestamp in [`Ticks`] (`now()`), and a [`Duration`] that defines the amount
-/// of time represented by a single tick of the clock.
-#[derive(Debug, Clone)]
 pub struct Clock {
-    now: fn() -> Ticks,
-    tick_duration: Duration,
     name: &'static str,
+    tick_duration: Duration,
+    clock: RawClock,
 }
 
-impl fmt::Display for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}, {:?} precision", self.name, self.tick_duration)
-    }
+/// A virtual function pointer table (vtable) that specifies the behavior
+/// of a [`RawClock`].
+///
+/// The pointer passed to all functions inside the vtable is the `data` pointer
+/// from the enclosing [`RawClock`] object.
+///
+/// The functions inside this struct are only intended to be called on the `data`
+/// pointer of a properly constructed [`RawClock`] object from inside the
+/// [`RawClock`] implementation. Calling one of the contained functions using
+/// any other `data` pointer will cause undefined behavior.
+///
+/// # Thread safety
+///
+/// All vtable functions must be thread-safe (even though [`RawClock`] is
+/// <code>\![Send] + \![Sync]</code>). This is because [`Clock`] is <code>[Send] + [Sync]</code>,
+/// and it *will* be moved to arbitrary threads or invoked by `&` reference. For example,
+/// this means that if the `clone` and `drop` functions manage a reference count,
+/// they must do so atomically.
+#[derive(Copy, Clone, Debug)]
+pub struct RawClockVTable {
+    clone: unsafe fn(*const ()) -> RawClock,
+    now: unsafe fn(*const ()) -> u64,
+    schedule_wakeup: unsafe fn(*const (), at: u64),
+    drop: unsafe fn(*const ()),
 }
+
+#[derive(Debug)]
+pub struct RawClock {
+    /// The `data` pointer can be used to store arbitrary data as required by the clock implementation.
+    data: *const (),
+    /// Virtual function pointer table that customizes the behavior of this clock.
+    vtable: &'static RawClockVTable,
+}
+
+// === impl Clock ===
+
+impl Unpin for Clock {}
+
+// Safety: As part of the safety contract for RawClockVTable, the caller promised RawClock is Send
+// therefore Clock is Send too
+unsafe impl Send for Clock {}
+// Safety: As part of the safety contract for RawClockVTable, the caller promised RawClock is Sync
+// therefore Clock is Sync too
+unsafe impl Sync for Clock {}
 
 impl Clock {
+    /// Creates a new `Clock` from the provided `tick_duration`, `data` pointer and `vtable`.
+    ///
+    /// The `tick_duration` is the `Duration` of time represented by a single `u64` tick in this clock.
+    /// This is in effect the precision of the clock and should be set to the precision of the underlying
+    /// hardware timer.
+    ///
+    /// The `data` pointer can be used to store arbitrary data as required by the clock implementation.
+    /// This could be e.g. a type-erased pointer to an `Arc` that holds private implementation-specific state.
+    /// The value of this pointer will get passed to all functions that are part
+    /// of the `vtable` as the first parameter.
+    ///
+    /// It is important to consider that the `data` pointer must point to a
+    /// thread safe type such as an `Arc`.
+    ///
+    /// The `vtable` customizes the behavior of a `Clock`. For each operation
+    /// on the `Clock`, the associated function in the `vtable` will be called.
+    ///
+    /// # Safety
+    ///
+    /// The behavior of the returned `Clock` is undefined if the contract defined
+    /// in [`RawClockVTable`]'s documentation is not upheld.
+    #[inline]
     #[must_use]
-    pub const fn new(tick_duration: Duration, now: fn() -> Ticks) -> Self {
+    pub const unsafe fn new(
+        tick_duration: Duration,
+        data: *const (),
+        vtable: &'static RawClockVTable,
+    ) -> Clock {
+        // Safety: ensured by caller
+        unsafe { Self::from_raw(tick_duration, RawClock { data, vtable }) }
+    }
+
+    /// Creates a new `Clock` from a [`RawClock`].
+    ///
+    /// # Safety
+    ///
+    /// The behavior of the returned `Waker` is undefined if the contract defined
+    /// in [`RawClock`]'s and [`RawClockVTable`]'s documentation is not upheld.
+    #[inline]
+    #[must_use]
+    pub const unsafe fn from_raw(tick_duration: Duration, clock: RawClock) -> Clock {
         Self {
-            now,
+            clock,
             tick_duration,
             name: "<unnamed mystery clock>",
         }
@@ -46,35 +114,9 @@ impl Clock {
     /// This is generally used to describe the hardware time source used by the
     /// `now()` function for this `Clock`.
     #[must_use]
-    pub const fn named(self, name: &'static str) -> Self {
-        Self { name, ..self }
-    }
-
-    /// Returns the current `now` timestamp, in [`Ticks`] of this clock's base
-    /// tick duration.
-    #[must_use]
-    pub(crate) fn now_ticks(&self) -> Ticks {
-        (self.now)()
-    }
-
-    /// Returns the [`Duration`] of one tick of this clock.
-    #[must_use]
-    pub fn tick_duration(&self) -> Duration {
-        self.tick_duration
-    }
-
-    /// Returns an [`Instant`] representing the current timestamp according to
-    /// this [`Clock`].
-    #[must_use]
-    pub fn now(&self) -> Instant {
-        let now = self.now_ticks();
-        Instant(self.ticks_to_duration(now))
-    }
-
-    /// Returns the maximum duration of this clock.
-    #[must_use]
-    pub fn max_duration(&self) -> Duration {
-        max_duration(self.tick_duration())
+    pub const fn named(mut self, name: &'static str) -> Self {
+        self.name = name;
+        self
     }
 
     /// Returns this `Clock`'s name, if it was given one using the [`Clock::named`]
@@ -84,54 +126,121 @@ impl Clock {
         self.name
     }
 
-    /// Convert the given raw [`Ticks`] into a [`Duration`] using this clocks
-    /// internal tick duration.
-    ///
-    /// # Panics
-    ///
-    /// This method panics if the conversion would overflow.
-    pub fn ticks_to_duration(&self, ticks: Ticks) -> Duration {
-        // Multiply nanoseconds as u64, because it cannot overflow that way.
-        let total_nanos = u64::from(self.tick_duration.subsec_nanos()) * ticks.0;
-        let extra_secs = total_nanos / (NANOS_PER_SEC);
-        let nanos = (total_nanos % (NANOS_PER_SEC)) as u32;
-        let Some(secs) = self.tick_duration.as_secs().checked_mul(ticks.0) else {
-            panic!(
-                "ticks_to_dur({:?}, {ticks:?}): multiplying tick \
-            duration seconds by ticks would overflow",
-                self.tick_duration
-            );
-        };
-        let Some(secs) = secs.checked_add(extra_secs) else {
-            panic!(
-                "ticks_to_dur({:?}, {ticks:?}): extra seconds from nanos ({extra_secs}s) would overflow total seconds",
-                self.tick_duration
-            )
-        };
-        debug_assert!(nanos < u32::try_from(NANOS_PER_SEC).unwrap());
-        Duration::new(secs, nanos)
+    /// Returns the [`Duration`] of one tick of this clock.
+    #[must_use]
+    pub const fn tick_duration(&self) -> Duration {
+        self.tick_duration
     }
 
-    /// Convert the given [`Duration`] into a raw [`Ticks`] using this clocks
-    /// internal tick duration.
-    ///
-    /// # Errors
-    ///
-    /// Returns a [`TimeError`] if the duration doesn't fit into the ticks u64 representation.
-    pub fn duration_to_ticks(&self, duration: Duration) -> Result<Ticks, TimeError> {
-        let raw: u64 = (duration.as_nanos() / self.tick_duration.as_nanos())
-            .try_into()
-            .map_err(|_| TimeError::DurationTooLong {
-                requested: self.tick_duration,
-                max: max_duration(self.tick_duration),
-            })?;
+    /// Returns the maximum duration of this clock.
+    #[must_use]
+    pub fn max_duration(&self) -> Duration {
+        max_duration(self.tick_duration())
+    }
 
-        Ok(Ticks(raw))
+    /// Gets the `data` pointer used to create this `Clock`.
+    #[inline]
+    #[must_use]
+    pub fn data(&self) -> *const () {
+        self.clock.data
+    }
+
+    /// Gets the `vtable` pointer used to create this `Clock`.
+    #[inline]
+    #[must_use]
+    pub fn vtable(&self) -> &'static RawClockVTable {
+        self.clock.vtable
+    }
+
+    #[inline]
+    pub fn now(&self) -> u64 {
+        // Safety: This is safe because `Clock::from_raw` is the only way
+        // to initialize `vtable` and `data` requiring the user to acknowledge
+        // that the contract of `RawClock` is upheld.
+        unsafe { (self.clock.vtable.now)(self.clock.data) }
+    }
+
+    #[inline]
+    pub fn schedule_wakeup(&self, at: u64) {
+        // Safety: see Clock::now
+        unsafe { (self.clock.vtable.schedule_wakeup)(self.clock.data, at) };
     }
 }
 
-#[inline]
-#[must_use]
-pub(super) fn max_duration(tick_duration: Duration) -> Duration {
-    tick_duration.saturating_mul(u32::MAX)
+impl Clone for Clock {
+    #[inline]
+    fn clone(&self) -> Self {
+        Clock {
+            // SAFETY: see Clock::now
+            clock: unsafe { (self.clock.vtable.clone)(self.clock.data) },
+            tick_duration: self.tick_duration,
+            name: self.name,
+        }
+    }
+}
+
+impl Drop for Clock {
+    #[inline]
+    fn drop(&mut self) {
+        // SAFETY: see Clock::now
+        unsafe { (self.clock.vtable.drop)(self.clock.data) }
+    }
+}
+
+impl fmt::Debug for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let vtable_ptr = self.clock.vtable as *const RawClockVTable;
+        f.debug_struct("Waker")
+            .field("name", &self.name)
+            .field("tick_duration", &self.tick_duration)
+            .field("data", &self.clock.data)
+            .field("vtable", &vtable_ptr)
+            .finish()
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}, {:?} precision", self.name, self.tick_duration)
+    }
+}
+
+// === impl RawClock ===
+
+impl RawClock {
+    /// Creates a new `Clock` from the provided `data` pointer and `vtable`.
+    ///
+    /// The `data` pointer can be used to store arbitrary data as required by the clock implementation.
+    /// his could be e.g. a type-erased pointer to an `Arc` that holds private implementation-specific state.
+    /// The value of this pointer will get passed to all functions that are part
+    /// of the `vtable` as the first parameter.
+    ///
+    /// It is important to consider that the `data` pointer must point to a
+    /// thread safe type such as an `Arc`.
+    ///
+    /// The `vtable` customizes the behavior of a `Clock`. For each operation
+    /// on the `Clock`, the associated function in the `vtable` will be called.
+    #[inline]
+    #[must_use]
+    pub const fn new(data: *const (), vtable: &'static RawClockVTable) -> RawClock {
+        Self { data, vtable }
+    }
+}
+
+// === impl RawClockVTable ===
+
+impl RawClockVTable {
+    pub const fn new(
+        clone: unsafe fn(*const ()) -> RawClock,
+        now: unsafe fn(*const ()) -> u64,
+        schedule_wakeup: unsafe fn(*const (), at: u64),
+        drop: unsafe fn(*const ()),
+    ) -> Self {
+        Self {
+            clone,
+            now,
+            schedule_wakeup,
+            drop,
+        }
+    }
 }

.github/workflows/ci.yml
@@ -97,7 +97,7 @@ jobs:
           github_access_token: ${{ secrets.GITHUB_TOKEN }}
       - uses: Swatinem/rust-cache@v2
       - name: just test-hosted
-        run: nix develop --command just test-hosted
+        run: nix develop --command just test
 
   test-miri:
     name: Miri Tests
@@ -113,7 +113,7 @@ jobs:
           github_access_token: ${{ secrets.GITHUB_TOKEN }}
       - uses: Swatinem/rust-cache@v2
       - name: just test-miri
-        run: nix develop --command just test-miri
+        run: nix develop --command just miri
 
   test-loom:
     name: Loom Tests
@@ -129,7 +129,7 @@ jobs:
           github_access_token: ${{ secrets.GITHUB_TOKEN }}
       - uses: Swatinem/rust-cache@v2
       - name: just test-loom
-        run: nix develop --command just test-loom
+        run: nix develop --command just loom
 
   test-riscv64:
     name: "On-Target Tests: RISCV 64-bit"

Cargo.toml
@@ -1,6 +1,6 @@
 [workspace]
 members = ["kernel", "libs/*", "build/*", "loader", "loader/api", "libs/ktest/macros"]
-resolver = "2"
+resolver = "3"
 
 [workspace.package]
 version = "0.1.0"
@@ -120,8 +120,9 @@ anyhow = { version = "1.0.98", default-features = false }
 loom = "0.7"
 criterion = "0.6.0"
 lazy_static = "1.5.0"
-pin-project = "1"
 cordyceps = "0.3.4"
+futures = { version = "0.3.31", default-features = false }
+pin-project = "1.1.10"
 
 # wast dependencies
 unicode-width = { version = "0.2.0" }

justfile
@@ -53,7 +53,7 @@ check crate="" *cargo_args="":
         {{ cargo_args }}
 
 # run all tests and checks
-preflight crate="" *cargo_args="": (lint crate cargo_args)
+preflight crate="" *cargo_args="": (lint crate cargo_args) (test crate cargo_args) (miri crate cargo_args) (loom crate cargo_args)
     typos
 
 # run lints (clippy, rustfmt, docs) for a crate or the entire for the workspace.
@@ -83,53 +83,43 @@ check-fmt crate="" *cargo_args="":
         {{ _fmt }} \
         {{ cargo_args }}
 
-# run all tests
-test crate="" *args="": \
-    (test-hosted crate args) \
-    (test-loom crate args) \
-    (test-miri crate args) \
-    (test-riscv64 crate args)
-
 # ==============================================================================
 # Hosted Testing
 # ==============================================================================
 
-# run WebAssembly spec tests
-test-spec:
-    {{ error("TODO") }}
-
 # crates that have hosted tests
-_hosted_crates := "-p kaddr2line -p cpu-local -p fastrand -p fdt -p kasync -p ksharded-slab -p spin -p wast@228.0.0 -p wavltree"
+_hosted_crates := "-p kaddr2line -p cpu-local -p fastrand -p fdt -p kasync --features counters -p ksharded-slab -p spin -p wast@228.0.0 -p wavltree"
 # run hosted tests
-test-hosted crate="" *args="": _get-nextest
+test crate="" *cargo_args="": _get-nextest
     {{ _cargo }} {{ _testcmd }} \
             {{ if crate == "" { _hosted_crates } else { "-p" + crate } }} \
             --lib \
             --no-fail-fast \
-            {{ args }}
+            {{ cargo_args }}
 
 # crates that have miri tests
-_miri_crates := "-p kasync -p ksharded-slab -p spin -p wavltree"
+_miri_crates := "-p kasync --features counters -p ksharded-slab -p spin -p wavltree"
 # run hosted tests under miri
-test-miri crate="" *args="": _get-nextest
-    MIRIFLAGS="{{ env_var_or_default("MIRIFLAGS", "-Zmiri-strict-provenance -Zmiri-disable-isolation") }} -Zmiri-env-forward=RUST_BACKTRACE" \
+miri crate="" *cargo_args="": _get-nextest
+    MIRIFLAGS="{{ env_var_or_default("MIRIFLAGS", "-Zmiri-strict-provenance -Zmiri-disable-isolation") }} -Zmiri-env-forward=RUST_BACKTRACE -Zmiri-env-forward=RUST_LOG" \
         RUSTFLAGS="{{ env_var_or_default("RUSTFLAGS", "-Zrandomize-layout") }}" \
         {{ _cargo }} miri {{ _testcmd }} \
             {{ if crate == "" { _miri_crates } else { "-p" + crate } }} \
             --lib \
             --no-fail-fast \
-            {{ args }}
+            {{ cargo_args }}
 
 # crates that have loom tests
-_loom_crates := "-p kasync"
+_loom_crates := "-p kasync --features counters"
 # run hosted tests under loom
-test-loom crate="" *args='': _get-nextest
+loom crate="" *cargo_args='': _get-nextest
     #!/usr/bin/env bash
     set -euo pipefail
     source "./util/shell.sh"
 
     export RUSTFLAGS="--cfg loom ${RUSTFLAGS:-}"
     export LOOM_LOG="${LOOM_LOG:-kasync=trace,cordyceps=trace,debug}"
+    export LOOM_MAX_PREEMPTIONS=2
 
     # if logging is enabled, also enable location tracking.
     if [[ "${LOOM_LOG}" != "off" ]]; then
@@ -155,7 +145,7 @@ test-loom crate="" *args='': _get-nextest
         {{ if crate == "" { _loom_crates } else { "-p" + crate } }} \
         --lib \
         --no-fail-fast \
-        {{ args }}
+        {{ cargo_args }}
     status="$?"
 
     if [[ "${LOOM_CHECKPOINT_FILE:-}" ]]; then

kernel/Cargo.toml
@@ -25,7 +25,7 @@ fastrand.workspace = true
 abort.workspace = true
 panic-unwind2.workspace = true
 util.workspace = true
-kasync = { workspace = true, features = ["panic-unwind2", "counters"] }
+kasync = { workspace = true, features = ["unwind2", "counters"] }
 
 # 3rd-party dependencies
 rustc-demangle.workspace = true

kernel/src/arch/riscv64/block_on.rs
@@ -1,0 +1,126 @@
+// Copyright 2025. Jonas Kruckenberg
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+use crate::state;
+use alloc::sync::Arc;
+use core::arch::asm;
+use core::mem::ManuallyDrop;
+use core::sync::atomic::{AtomicBool, Ordering};
+use core::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
+use cpu_local::cpu_local;
+use futures::pin_mut;
+use futures::task::WakerRef;
+use riscv::sbi;
+
+struct HartNotify {
+    hartid: usize,
+    unparked: AtomicBool,
+}
+
+impl HartNotify {
+    fn wake_by_ref(me: &Arc<Self>) {
+        tracing::trace!("waking up hart {}...", me.hartid);
+
+        // Make sure the wakeup is remembered until the next `park()`.
+        let unparked = me.unparked.swap(true, Ordering::Release);
+        if !unparked {
+            // If the thread has not been unparked yet, it must be done
+            // now. If it was actually parked, it will run again,
+            // otherwise the token made available by `unpark`
+            // may be consumed before reaching `park()`, but `unparked`
+            // ensures it is not forgotten.
+            sbi::ipi::send_ipi(1 << me.hartid, 0).unwrap();
+        }
+    }
+}
+
+fn waker_ref(wake: &Arc<HartNotify>) -> WakerRef<'_> {
+    static WAKER_VTABLE: RawWakerVTable = RawWakerVTable::new(
+        clone_arc_raw,
+        wake_arc_raw,
+        wake_by_ref_arc_raw,
+        drop_arc_raw,
+    );
+
+    unsafe fn clone_arc_raw(data: *const ()) -> RawWaker {
+        // Safety: ensured by caller
+        unsafe { Arc::increment_strong_count(data.cast::<HartNotify>()) }
+        RawWaker::new(data, &WAKER_VTABLE)
+    }
+
+    unsafe fn wake_arc_raw(data: *const ()) {
+        // Safety: the `data` pointer is created through `Arc::as_ptr` below.
+        let arc = unsafe { Arc::from_raw(data.cast::<HartNotify>()) };
+        HartNotify::wake_by_ref(&arc);
+    }
+
+    unsafe fn wake_by_ref_arc_raw(data: *const ()) {
+        // Retain Arc, but don't touch refcount by wrapping in ManuallyDrop
+        // Safety: the `data` pointer is created through `Arc::as_ptr` below.
+        let arc = ManuallyDrop::new(unsafe { Arc::from_raw(data.cast::<HartNotify>()) });
+        HartNotify::wake_by_ref(&arc);
+    }
+
+    unsafe fn drop_arc_raw(data: *const ()) {
+        // Safety: the `data` pointer is created through `Arc::as_ptr` below.
+        drop(unsafe { Arc::from_raw(data.cast::<HartNotify>()) });
+    }
+
+    // simply copy the pointer instead of using Arc::into_raw,
+    // as we don't actually keep a refcount by using ManuallyDrop.<
+    let ptr = Arc::as_ptr(wake).cast::<()>();
+
+    // Safety: TODO
+    let waker = ManuallyDrop::new(unsafe { Waker::from_raw(RawWaker::new(ptr, &WAKER_VTABLE)) });
+    WakerRef::new_unowned(waker)
+}
+
+/// Blocks the calling hart until the give future completes and yielding its
+/// resolved result.
+///
+/// Any tasks or timers which the future spawns internally, will **not** be executed unless there
+/// are running executor workers available. This makes this function mostly suitable to drive
+/// the top-level main loop future returned by `Worker::run`, since that will take care of
+/// driving the tasks and timers to completion.
+///
+/// When the given future cannot make progress and returned `Poll::Pending`, the calling hart
+/// will automatically be put into a low-power suspend state to conserve energy by using the builtin
+/// "wait for interrupt" (`wfi`) instruction until woken by the [`Waker`]. When using this function
+/// to drive the worker loop the `Waker` should be called for event that might result in tasks becoming
+/// unblocked such as interrupts and "wakeup calls" from other harts.
+pub fn block_on<F: Future>(f: F) -> F::Output {
+    pin_mut!(f);
+
+    cpu_local! {
+        static CURRENT_THREAD_NOTIFY: Arc<HartNotify> = Arc::new(HartNotify {
+            hartid: state::cpu_local().id,
+            unparked: AtomicBool::new(false),
+        });
+    }
+
+    let waker = waker_ref(&CURRENT_THREAD_NOTIFY);
+    let mut cx = Context::from_waker(&waker);
+    loop {
+        if let Poll::Ready(t) = f.as_mut().poll(&mut cx) {
+            return t;
+        }
+
+        // Wait for a wakeup.
+        while !CURRENT_THREAD_NOTIFY
+            .unparked
+            .swap(false, Ordering::Acquire)
+        {
+            // No wakeup occurred. It may occur now, right before parking,
+            // but in that case the token made available by `unpark()`
+            // is guaranteed to still be available and `park()` is a no-op.
+            tracing::trace!("parking hart {}", state::cpu_local().id);
+            // Safety: inline assembly
+            unsafe { asm!("wfi") };
+            tracing::trace!("hart {} woke up", state::cpu_local().id);
+        }
+    }
+}

kernel/src/arch/riscv64/device/cpu.rs
@@ -12,9 +12,6 @@ use bitflags::bitflags;
 use core::cell::RefCell;
 use core::fmt;
 use core::str::FromStr;
-use core::time::Duration;
-use kasync::time::Ticks;
-use kasync::time::{Clock, NANOS_PER_SEC};
 
 #[derive(Debug)]
 pub struct Cpu {
@@ -23,7 +20,7 @@ pub struct Cpu {
     pub cboz_block_size: Option<usize>,
     pub cbom_block_size: Option<usize>,
     pub plic: RefCell<device::plic::Plic>,
-    pub clock: Clock,
+    pub clock: kasync::time::Clock,
 }
 
 bitflags! {
@@ -109,12 +106,6 @@ impl Cpu {
             })
             .expect("CPU node not found in device tree");
 
-        let timebase_frequency = cpu
-            .property("timebase-frequency")
-            .or_else(|| cpu.parent().unwrap().property("timebase-frequency"))
-            .unwrap()
-            .as_u64()?;
-
         let cbop_block_size = cpu
             .property("riscv,cbop-block-size")
             .map(|prop| prop.as_usize().unwrap());
@@ -140,17 +131,7 @@ impl Cpu {
         let mut plic = device::plic::Plic::new(devtree, hlic_node)?;
         plic.irq_unmask(10);
 
-        let tick_duration = Duration::from_nanos(NANOS_PER_SEC / timebase_frequency);
-        let clock = Clock::new(tick_duration, || Ticks(riscv::register::time::read64()));
-
-        debug_assert_eq!(
-            clock.ticks_to_duration(Ticks(timebase_frequency)),
-            Duration::from_secs(1)
-        );
-        debug_assert_eq!(
-            clock.duration_to_ticks(Duration::from_secs(1)).unwrap(),
-            Ticks(timebase_frequency)
-        );
+        let clock = device::clock::new(cpu)?;
 
         Ok(Self {
             clock,

commit f9598d7f34e2196f744661e2beffbfcd6eb9609e

fix: kernel hang with `log=trace`
This changes fixes a kernel hang (or rather infinite loop) introduced in 1656fcf50a1d2d35569ef5710ebccde45d48f654 when the new RISCV clock implementation started
printing trace messages whie getting the current time. https://github.com/JonasKruckenberg/k23/commit/1656fcf50a1d2d35569ef5710ebccde45d48f654#diff-6011d78af5dae4cd359c21ca15b8566420c3f10a2406d9b4c461df2faaa414bdR27-R30
This leads to a recursive print-loop because getting the time is part of printing debug messages

kernel/src/arch/riscv64/device/clock.rs
@@ -17,39 +17,23 @@ static CLOCK_VTABLE: RawClockVTable =
     RawClockVTable::new(clone_raw, now_raw, schedule_wakeup_raw, drop_raw);
 
 unsafe fn clone_raw(ptr: *const ()) -> RawClock {
-    tracing::trace!(
-        clock.addr = ?ptr,
-        "RISCV CLOCK::clone_raw"
-    );
     debug_assert!(ptr.is_null());
     RawClock::new(ptr, &CLOCK_VTABLE)
 }
 
 unsafe fn now_raw(_ptr: *const ()) -> u64 {
-    tracing::trace!(
-        clock.addr = ?_ptr,
-        "RISCV CLOCK::now_raw"
-    );
     debug_assert!(_ptr.is_null());
 
     riscv::register::time::read64()
 }
 
 unsafe fn schedule_wakeup_raw(_ptr: *const (), at: u64) {
-    tracing::trace!(
-        clock.addr = ?_ptr,
-        "RISCV CLOCK::schedule_wakeup_raw"
-    );
     debug_assert!(_ptr.is_null());
 
     sbi::time::set_timer(at).unwrap();
 }
 
 unsafe fn drop_raw(_ptr: *const ()) {
-    tracing::trace!(
-        clock.addr = ?_ptr,
-        "RISCV CLOCK::drop_raw"
-    );
     debug_assert!(_ptr.is_null());
 }
 