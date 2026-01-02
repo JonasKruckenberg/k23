3fe6c9b66db6c8437d1626ba13a9c7511308467b
Jonas Kruckenberg <iterpre@protonmail.com>
2026-01-02 16:44:17 +0100

    fix(manual): use mdbook from nix in CI (#629)

--- flake.nix
+++ flake.nix
@@ -5,19 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
-        url = "github:oxalica/rust-overlay";
-        inputs = {
-            nixpkgs.follows = "nixpkgs";
-        };
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
         rustToolchain = with pkgs; rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in
